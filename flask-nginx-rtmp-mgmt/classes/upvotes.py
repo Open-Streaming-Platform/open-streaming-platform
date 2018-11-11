325e4a359d0056eb6908252ceb21df872d3aee34
David Lockwood <dave.r.lockwood@gmail.com>
2018-11-11 18:59:47 -0500

    Adding initial upvote system

---
+++ flask-nginx-rtmp-mgmt/classes/upvotes.py
@@ -0,0 +1,40 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+from app import db
+
+class channelUpvotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer)
+    channelID = db.Column(db.Integer)
+
+    def __init__(self, userID, channelID):
+        self.userID = userID
+        self.channelID = channelID
+
+    def __repr__(self):
+        return '<id %r>' % self.id
+
+class streamUpvotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer)
+    streamID = db.Column(db.Integer)
+
+    def __init__(self, userID, channelID):
+        self.userID = userID
+        self.channelID = channelID
+
+    def __repr__(self):
+        return '<id %r>' % self.id
+
+class videoUpvotes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer)
+    channelID = db.Column(db.Integer)
+
+    def __init__(self, userID, channelID):
+        self.userID = userID
+        self.channelID = channelID
+
+    def __repr__(self):
+        return '<id %r>' % self.id
