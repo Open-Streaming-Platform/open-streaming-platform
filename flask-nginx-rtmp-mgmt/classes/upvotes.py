24eef8f281e9d6e48781e71ff477f8aa7a9ef4cb
David Lockwood <dave.r.lockwood@gmail.com>
2018-11-23 12:48:09 -0500

    Added Upvotes to API Query for Videos

--- flask-nginx-rtmp-mgmt/classes/upvotes.py
+++ flask-nginx-rtmp-mgmt/classes/upvotes.py
@@ -6,7 +6,7 @@ from app import db
 class channelUpvotes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userID = db.Column(db.Integer)
-    channelID = db.Column(db.Integer)
+    channelID = db.Column(db.Integer, db.ForeignKey('Channel.id'))
 
     def __init__(self, userID, channelID):
         self.userID = userID
@@ -18,7 +18,7 @@ class channelUpvotes(db.Model):
 class streamUpvotes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userID = db.Column(db.Integer)
-    streamID = db.Column(db.Integer)
+    streamID = db.Column(db.Integer, db.ForeignKey('Stream.id'))
 
     def __init__(self, userID, streamID):
         self.userID = userID
@@ -30,7 +30,7 @@ class streamUpvotes(db.Model):
 class videoUpvotes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userID = db.Column(db.Integer)
-    videoID = db.Column(db.Integer)#,db.ForeignKey('RecordedVideo.id'))
+    videoID = db.Column(db.Integer, db.ForeignKey('RecordedVideo.id'))
 
     def __init__(self, userID, channelID):
         self.userID = userID
