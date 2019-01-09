8e7307c80738fbd9444f587789b68e482deeff4a
David Lockwood <dave.r.lockwood@gmail.com>
2019-01-09 10:35:00 -0500

    Added Flask-Migrate Requirements for Future DB Updates

---
+++ flask-nginx-rtmp-mgmt/migrate.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app import app, db
+
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
