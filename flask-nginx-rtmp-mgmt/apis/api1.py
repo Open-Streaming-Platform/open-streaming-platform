82606c419a37cd3c7b1c32965a1307768182e89b
David Lockwood <dave.r.lockwood@gmail.com>
2019-01-13 17:13:48 -0500

    Fixes for API Imports

--- flask-nginx-rtmp-mgmt/apis/api1.py
+++ flask-nginx-rtmp-mgmt/apis/api1.py
@@ -3,10 +3,10 @@ from flask_restplus import Api, Resource
 
 import json
 
-from ..app import Channel
-from ..app import Stream
-from ..app import RecordedVideo
-from ..app import topics
+from .. import Channel
+from .. import Stream
+from .. import RecordedVideo
+from .. import topics
 
 
 blueprint = Blueprint('api', __name__)
