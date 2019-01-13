cbf2c8d51013c3811b59eaeeff3a1db6b8220a84
David Lockwood <dave.r.lockwood@gmail.com>
2019-01-13 14:45:13 -0500

    Fix for API interfering

---
+++ flask-nginx-rtmp-mgmt/apis/api1.py
@@ -0,0 +1,59 @@
+from flask import Blueprint
+from flask_restplus import Api, Resource
+from ..app import Channel
+from ..app import Stream
+from ..app import RecordedVideo
+from ..app import topics
+
+
+blueprint = Blueprint('api', __name__)
+api = Api(blueprint, doc='/api/doc/')
+
+### Start API Functions ###
+@api.route('/channels/')
+class api_1_ListChannels(Resource):
+    def get(self):
+        channelList = Channel.Channel.query.all()
+        return json.dumps({'results': [ob.serialize() for ob in channelList]})
+
+@api.route('/channels/<string:channelEndpointID>')
+class api_1_ListChannel(Resource):
+    def get(self, channelEndpointID):
+        channelList = Channel.Channel.query.filter_by(channelLoc=channelEndpointID).all()
+        return json.dumps({'results': [ob.serialize() for ob in channelList]})
+
+@api.route('/streams')
+class api_1_ListStreams(Resource):
+    def get(self):
+        streamList = Stream.Stream.query.all()
+        return json.dumps({'results': [ob.serialize() for ob in streamList]})
+
+@api.route('/streams/<int:streamID>')
+class api_1_ListStream(Resource):
+    def get(self, streamID):
+        streamList = Stream.Stream.query.filter_by(id=streamID).all()
+        return json.dumps({'results': [ob.serialize() for ob in streamList]})
+
+@api.route('/vids')
+class api_1_ListVideos(Resource):
+    def get(self):
+        videoList = RecordedVideo.RecordedVideo.query.all()
+        return json.dumps({'results': [ob.serialize() for ob in videoList]})
+
+@api.route('/vids/<int:videoID>')
+class api_1_ListVideo(Resource):
+    def get(self, videoID):
+        videoList = RecordedVideo.RecordedVideo.query.filter_by(id=videoID).all()
+        return json.dumps({'results': [ob.serialize() for ob in videoList]})
+
+@api.route('/topics')
+class api_1_ListTopics(Resource):
+    def get(self):
+        topicList = topics.topics.query.all()
+        return json.dumps({'results': [ob.serialize() for ob in topicList]})
+
+@api.route('/topics/<int:topicID>')
+class api_1_ListTopic(Resource):
+    def get(self, topicID):
+        topicList = topics.topics.query.filter_by(id=topicID).all()
+        return json.dumps({'results': [ob.serialize() for ob in topicList]})
