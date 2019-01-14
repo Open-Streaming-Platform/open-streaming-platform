30b9eea497628658f5491fd858204320f6562c85
David Lockwood <dave.r.lockwood@gmail.com>
2019-01-13 20:00:50 -0500

    Endpoint Fixes

--- flask-nginx-rtmp-mgmt/apiv1.py
+++ flask-nginx-rtmp-mgmt/apiv1.py
@@ -13,9 +13,8 @@ from classes import RecordedVideo
 from classes import topics
 
 
-api_v1 = Blueprint('api', __name__, version='1.0', title='OSP API',
-    description='OSP API for Users, Streamers, and Admins', url_prefix='/api/1')
-api = Api(api_v1, doc='/doc/')
+api_v1 = Blueprint('api', __name__, url_prefix='/api/1')
+api = Api(api_v1, doc='/doc/', version='1.0', title='OSP API', description='OSP API for Users, Streamers, and Admins')
 
 ### Start API Functions ###
 @api.route('/channels/')
