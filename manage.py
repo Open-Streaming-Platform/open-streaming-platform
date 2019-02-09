3409ecc035af52858102585046999a0890853f10
David Lockwood <dave.r.lockwood@gmail.com>
2019-02-09 10:35:33 -0500

    Fixes for Migrate Script

--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
-from flask_migrate import Migrate, MigrateCommand
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
+from conf import config
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = config.dbLocation
+
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+from classes import Stream
+from classes import Channel
+from classes import dbVersion
+from classes import RecordedVideo
+from classes import topics
+from classes import settings
+from classes import banList
+from classes import Sec
+from classes import upvotes
+from classes import apikey
 
 if __name__ == '__main__':
     manager.run()
