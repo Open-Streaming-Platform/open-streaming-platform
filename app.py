b0dcc2b2c1e45d4bd5222288d131d092022d348d
David Lockwood <dave.r.lockwood@gmail.com>
2019-04-30 18:32:52 -0400

    Debug

--- app.py
+++ app.py
@@ -287,27 +287,25 @@ def runWebhook(channelID, triggerType, **kwargs):
     f = open('/opt/osp/log.txt', 'w')
     webhookQuery = webhook.webhook.query.filter_by(channelID=channelID, requestTrigger=triggerType).all()
 
-    try:
-        if webhookQuery != []:
-            for hook in webhookQuery:
-                url = hook.endpointURL
-                payload = processWebhookVariables(hook.requestPayload, **kwargs)
-                header = json.loads(hook.requestHeader)
-                requestType = hook.requestType
-                try:
-                    if requestType == 0:
-                        r = requests.post(url, headers=header, data=payload)
-                    elif requestType == 1:
-                        r = requests.get(url, headers=header, data=payload)
-                    elif requestType == 2:
-                        r = requests.put(url, headers=header, data=payload)
-                    elif requestType == 3:
-                        r = requests.delete(url, headers=header, data=payload)
-                    str(f.write(r))
-                except:
-                    pass
-    except Exception as ex:
-        f.write(str(ex))
+    if webhookQuery != []:
+        for hook in webhookQuery:
+            url = hook.endpointURL
+            payload = processWebhookVariables(hook.requestPayload, **kwargs)
+            header = json.loads(hook.requestHeader)
+            requestType = hook.requestType
+            try:
+                if requestType == 0:
+                    r = requests.post(url, headers=header, data=payload)
+                elif requestType == 1:
+                    r = requests.get(url, headers=header, data=payload)
+                elif requestType == 2:
+                    r = requests.put(url, headers=header, data=payload)
+                elif requestType == 3:
+                    r = requests.delete(url, headers=header, data=payload)
+                str(f.write(r))
+            except:
+                pass
+
     f.close()
 
 def processWebhookVariables(payload, **kwargs):
