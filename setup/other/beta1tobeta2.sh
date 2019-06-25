a6a5bd9d018128ddc08f26fcdd14dab32f4b3faa
David Lockwood <dave.r.lockwood@gmail.com>
2019-06-24 20:27:21 -0400

    Added Beta1 to Beta 2 Upgrade Script

---
+++ setup/other/beta1tobeta2.sh
@@ -0,0 +1,7 @@
+pip3 install -r /opt/osp/setup/requirements.txt
+sudo mkdir -p /opt/osp/vapid
+sudo chmod -R 774 /opt/osp/vapid
+sudo openssl ecparam -name prime256v1 -genkey -noout -out /opt/osp/vapid/vapid_private.pem
+sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -pubout -out /opt/osp/vapid/vapid_public.pem
+sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -outform DER|tail -c +8|head -c 32|base64|tr -d '=' |tr '/+' '_-' >> /opt/osp/vapid/private_key.txt
+sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -pubout -outform DER|tail -c 65|base64|tr -d '=' |tr '/+' '_-' >> /opt/osp/vapid/public_key.txt
