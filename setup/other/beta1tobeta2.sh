6d8faf188fd7c2c4472f378941d81f6921fe2cf4
David Lockwood <dave.r.lockwood@gmail.com>
2019-06-24 20:53:54 -0400

    Fixes for Key Generation

--- setup/other/beta1tobeta2.sh
+++ setup/other/beta1tobeta2.sh
@@ -4,5 +4,5 @@ sudo chown -R www-data:www-data /opt/osp/vapid
 sudo chmod -R 774 /opt/osp/vapid
 sudo openssl ecparam -name prime256v1 -genkey -noout -out /opt/osp/vapid/vapid_private.pem
 sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -pubout -out /opt/osp/vapid/vapid_public.pem
-sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -outform DER|tail -c +8|head -c 32|base64|tr -d '=' |tr -d '\n' |tr '/+' '_-' >> /opt/osp/vapid/private_key.txt
-sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -pubout -outform DER|tail -c 65|base64|tr -d '=' | tr -d '\n' |tr '/+' '_-' >> /opt/osp/vapid/public_key.txt
+sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -outform DER|tail -c +8|head -c 32|base64|tr -d '=' |tr -d '\n' |tr '/+' '_-' > /opt/osp/vapid/private_key.txt
+sudo openssl ec -in /opt/osp/vapid/vapid_private.pem -pubout -outform DER|tail -c 65|base64|tr '/+' '_-'|tr -d '\n' > /opt/osp/vapid/public_key.txt
