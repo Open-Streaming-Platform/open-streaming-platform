2e7b3e3447cff4187e5c5e2917a7d9f238e333fc
David Lockwood <dave.r.lockwood@gmail.com>
2019-07-14 21:31:43 -0400

    Fixes

--- setup/other/alpha4tobeta1.sh
+++ setup/other/alpha4tobeta1.sh
@@ -1,7 +1,6 @@
 pip3 install -r /opt/osp/setup/requirements.txt
 cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.old
 cp /opt/osp/setup/nginx/nginx.conf /usr/local/nginx/conf/nginx.conf
-I
 sudo mkdir /var/www/live-adapt
 sudo chown -R www-data:www-data /var/www/live-adapt
 sudo mkdir /var/www/stream-thumb
