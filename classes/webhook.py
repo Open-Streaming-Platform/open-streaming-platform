fc5a50af866d18e7fadad39ef028e38debf90967
David Lockwood <dave.r.lockwood@gmail.com>
2019-08-24 11:50:30 -0400

    Remove Unused Column

--- classes/webhook.py
+++ classes/webhook.py
@@ -25,7 +25,6 @@ class webhook(db.Model):
 class globalWebhook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
-    channelID = db.Column(db.Integer, db.ForeignKey('Channel.id'))
     endpointURL = db.Column(db.String(2048))
     requestHeader = db.Column(db.String(4096))
     requestPayload = db.Column(db.String(4096))
