2f69fbcbfce1e24ec6c362e4b4ffae988d1496b5
David Lockwood <dave.r.lockwood@gmail.com>
2019-09-17 21:16:55 -0400

    Fixes to Quotes

--- app.py
+++ app.py
@@ -507,7 +507,7 @@ def prepareHubJSON():
     topicQuery = topics.topics.query.all()
     topicDump = {}
     for topic in topicQuery:
-        topicDump[topic.id] = {'name': topic.name, 'img': topic.iconClass}
+        topicDump[topic.id] = {"name": topic.name, "img": topic.iconClass}
 
     streamerIDs = []
     for channel in db.session.query(Channel.Channel.owningUser).distinct():
@@ -517,40 +517,40 @@ def prepareHubJSON():
     streamerDump = {}
     for streamerID in streamerIDs:
         streamerQuery = Sec.User.query.filter_by(id=streamerID).first()
-        streamerDump[streamerQuery.id] = {'username': streamerQuery.username, 'biography': streamerQuery.biography,
-                                          'img': streamerQuery.pictureLocation}
+        streamerDump[streamerQuery.id] = {"username": streamerQuery.username, "biography": streamerQuery.biography,
+                                          "img": streamerQuery.pictureLocation}
 
     channelDump = {}
     channelQuery = Channel.Channel.query.all()
     for channel in channelQuery:
-        channelDump[channel.id] = {'streamer': channel.owningUser, 'name': channel.channelName,
-                                   'location': channel.channelLoc, 'topic': channel.topic, 'views': channel.views,
-                                   'protected': channel.protected,
-                                   'currentViewers': channel.currentViewers, 'img': channel.imageLocation,
-                                   'description': channel.description}
+        channelDump[channel.id] = {"streamer": channel.owningUser, "name": channel.channelName,
+                                   "location": channel.channelLoc, "topic": channel.topic, "views": channel.views,
+                                   "protected": channel.protected,
+                                   "currentViewers": channel.currentViewers, "img": channel.imageLocation,
+                                   "description": channel.description}
 
     videoDump = {}
     videoQuery = RecordedVideo.RecordedVideo.query.filter_by(pending=False).all()
     for video in videoQuery:
-        videoDump[video.id] = {'streamer': video.owningUser, 'name': video.channelName, 'channelID': video.channelID,
-                               'description': video.description, 'topic': video.topic, 'views': video.views,
-                               'length': video.length, 'location': video.videoLocation, 'img': video.thumbnailLocation}
+        videoDump[video.id] = {"streamer": video.owningUser, "name": video.channelName, "channelID": video.channelID,
+                               "description": video.description, "topic": video.topic, "views": video.views,
+                               "length": video.length, "location": video.videoLocation, "img": video.thumbnailLocation}
 
     clipDump = {}
     clipQuery = RecordedVideo.Clips.query.all()
     for clip in clipQuery:
-        clipDump[clip.id] = {'parentVideo': clip.parentVideo, 'length': clip.length, 'views': clip.views,
-                             'name': clip.clipName, 'description': clip.description, 'img': clip.thumbnailLocation}
+        clipDump[clip.id] = {"parentVideo": clip.parentVideo, "length": clip.length, "views": clip.views,
+                             "name": clip.clipName, "description": clip.description, "img": clip.thumbnailLocation}
 
     streamDump = {}
     streamQuery = Stream.Stream.query.all()
     for stream in streamQuery:
-        streamDump[stream.id] = {'channelID': stream.linkedChannel, 'url': ('/view/' + stream.channel.channelLoc + '/'),
-                                 'name': stream.streamName, 'topic': stream.topic,
-                                 'currentViewers': stream.currentViewers, 'views': stream.totalViewers}
+        streamDump[stream.id] = {"channelID": stream.linkedChannel, "url": ("/view/" + stream.channel.channelLoc + "/"),
+                                 "name": stream.streamName, "topic": stream.topic,
+                                 "currentViewers": stream.currentViewers, "views": stream.totalViewers}
 
-    dataDump = {'topics': topicDump, 'streamers': streamerDump, 'channels': channelDump, 'videos': videoDump,
-                'clips': clipDump, 'streams': streamDump}
+    dataDump = {"topics": topicDump, "streamers": streamerDump, "channels": channelDump, "videos": videoDump,
+                "clips": clipDump, "streams": streamDump}
     db.session.close()
     return dataDump
 
