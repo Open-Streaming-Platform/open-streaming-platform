bb2152d77f743d4fa62774a56e053ff8e8d223a1
David Lockwood <dave.r.lockwood@gmail.com>
2019-09-28 21:53:30 -0400

    Search fixes

--- app.py
+++ app.py
@@ -2943,7 +2943,7 @@ def search_page():
         streamerList1 = Sec.User.query.filter(Sec.User.username.contains(search)).all()
         streamerList2 = Sec.User.query.filter(Sec.User.biography.contains(search)).all()
         for stream in streamerList1:
-            if stream.has_role('streamer'):
+            if stream.has_role('Streamer'):
                 streamerList.append(stream)
         for stream in streamerList2:
             if stream not in streamerList and stream.has_role('streamer'):
