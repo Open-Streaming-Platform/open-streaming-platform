264de86002e318161c7826cfc5fa3173b5dfa403
David Lockwood <dave.r.lockwood@gmail.com>
2019-11-24 14:28:58 -0500

    Added List Clear Logic for Notifications

--- app.py
+++ app.py
@@ -3297,7 +3297,7 @@ def search_page():
 @login_required
 @app.route('/notifications')
 def notification_page():
-    notificationQuery = notifications.userNotification.query.filter_by(userID=current_user.id).order_by(notifications.userNotification.timestamp.desc())
+    notificationQuery = notifications.userNotification.query.filter_by(userID=current_user.id, read=False).order_by(notifications.userNotification.timestamp.desc())
     return render_template(checkOverride('notifications.html'), notificationList=notificationQuery)
 
 ### Start Video / Stream Handler Routes
