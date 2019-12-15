fad18e26f07daff32acaa9ffff696b4444cf871d
David Lockwood <dave.r.lockwood@gmail.com>
2019-12-14 20:43:05 -0500

    Added migration for flask-security

--- setup/other/beta3tobeta4.sh
+++ setup/other/beta3tobeta4.sh
@@ -6,6 +6,8 @@ cd ..
 sudo cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.old
 sudo bash setup-osp.sh
 sudo pip3 install gunicorn
+sudo pip3 uninstall flask-security
+sudo pip3 install flask-security-too
 cd ..
 sudo python3 manage.py db init
 sudo bash dbUpgrade.sh
