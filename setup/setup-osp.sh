f7f8f63eae776e75f49c543f6ca44bd73f61b163
David Lockwood <dave.r.lockwood@gmail.com>
2020-01-11 07:35:29 -0500

    Added Redis tweak for optimization

--- setup/setup-osp.sh
+++ setup/setup-osp.sh
@@ -11,6 +11,8 @@ sudo pip3 install -r requirements.txt
 
 # Install Redis
 sudo apt-get install redis -y
+sed -i 's/appendfsync everysec/appendfsync no/' /etc/redis/redis.conf
+sudo systemctl restart redis
 
 # Setup OSP Directory
 mkdir -p /opt/osp
