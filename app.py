5881a700cfe9dae0e7246c732bf23a5b5dbee5c4
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-05 14:27:19 -0400

    Attempting Reduced Size Query

--- app.py
+++ app.py
@@ -1235,7 +1235,7 @@ def main_page():
     else:
         activeStreams = Stream.Stream.query.order_by(Stream.Stream.currentViewers).all()
 
-        randomRecorded = RecordedVideo.RecordedVideo.query.filter_by(pending=False, published=True).join(Sec.User).join(Channel.Channel).with_entities(RecordedVideo.RecordedVideo.id, RecordedVideo.RecordedVideo.views,
+        randomRecorded = RecordedVideo.RecordedVideo.query.filter_by(pending=False, published=True).join(Channel.Channel, RecordedVideo.RecordedVideo.channelID == Channel.Channel.id).join(Sec.User, RecordedVideo.RecordedVideo.owningUser == Sec.User.id).with_entities(RecordedVideo.RecordedVideo.id, RecordedVideo.RecordedVideo.views,
                                                                                                                   RecordedVideo.RecordedVideo.length, RecordedVideo.RecordedVideo.thumbnailLocation,
                                                                                                                   RecordedVideo.RecordedVideo.channelName, RecordedVideo.RecordedVideo.topic,
                                                                                                                   RecordedVideo.RecordedVideo.videoDate, Sec.User.pictureLocation, Channel.Channel.protected).order_by(func.random()).limit(16)
