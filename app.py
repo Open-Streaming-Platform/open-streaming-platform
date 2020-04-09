aafad1c51733402e60990798202a89768e8815af
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-09 07:46:18 -0400

    Changes to reduce latency with Edge Streamer on Internet

--- app.py
+++ app.py
@@ -3785,7 +3785,8 @@ def user_auth_check():
                 edgeRestreamSubprocesses[requestedChannel.channelLoc] = []
 
                 for node in ospEdgeNodeQuery:
-                    subprocessConstructor = ["ffmpeg", "-i", inputLocation, "-c", "copy", "-c:v", "libx264", "-g", "1", "-keyint_min", "1", "-x264opts", "no-scenecut", "-bufsize", "6000k", "-c:a", "aac", "-b:a", "160k", "-ac", "2"]
+                    #subprocessConstructor = ["ffmpeg", "-i", inputLocation, "-c", "copy", "-c:v", "libx264", "-g", "1", "-keyint_min", "1", "-x264opts", "no-scenecut", "-bufsize", "6000k", "-c:a", "aac", "-b:a", "160k", "-ac", "2"]
+                    subprocessConstructor = ["ffmpeg", "-i", inputLocation, "-c", "copy"]
                     subprocessConstructor.append("-f")
                     subprocessConstructor.append("flv")
                     if sysSettings.adaptiveStreaming:
