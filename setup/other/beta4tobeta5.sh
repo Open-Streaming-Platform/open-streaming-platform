d90d6485c4baec0a2e27d253da1a711adddbb161
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-15 15:35:57 -0400

    Added Beta 4 to Beta 5 Upgrade Script

---
+++ setup/other/beta4tobeta5.sh
@@ -0,0 +1,14 @@
+loglocation=/opt/osp/logs/beta5Upgrade.log
+
+echo Creating Backup of /usr/local/nginx/conf/nginx.conf as /usr/local/nginx/conf/nginx.conf.old
+sudo cp /usr/local/nginx/conf/nginx.conf /usr/local/nginx/conf/nginx.conf.old > $loglocation
+echo Replacing Nginx Conf Files
+sudo cp /opt/osp/setup/nginx/*.conf /usr/local/nginx/conf/
+echo Setting Ownership of /opt/osp to www-data
+sudo chown -R www-data:www-data /opt/osp >> $loglocation
+echo Performing DB Migration
+sudo python3 manage.py db init >> $loglocation
+sudo bash dbUpgrade.sh >> $loglocation
+sudo systemctl restart osp.target >> $loglocation
+sudo systemctl restart nginx >> $loglocation
+echo Upgrade Completed.  Please check $loglocation for any errors.
