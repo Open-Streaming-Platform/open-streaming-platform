a90e64f957cc7103ab5dfb9cc57c3a37227f3a6b
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-18 21:12:12 -0400

    Quieting output

--- osp-config.sh
+++ osp-config.sh
@@ -29,20 +29,20 @@ display_result() {
 upgrade_osp() {
    UPGRADELOG="/opt/osp/logs/upgrade.log"
    echo 0 | dialog --title "Upgrading OSP" --gauge "Pulling Git Repo" 10 70 0
-   git stash > $UPGRADELOG
-   git pull >> $UPGRADELOG
+   git stash > $UPGRADELOG 2>&1
+   git pull >> $UPGRADELOG 2>&1
    echo 15 | dialog --title "Upgrading OSP" --gauge "Setting /opt/osp Ownership" 10 70 0
-   chown -R $http_user:$http_user /opt/osp >> $UPGRADELOG
+   chown -R $http_user:$http_user /opt/osp >> $UPGRADELOG 2>&1
    echo 25 | dialog --title "Upgrading OSP" --gauge "Stopping OSP" 10 70 0
-   systemctl stop osp.target >> $UPGRADELOG
+   systemctl stop osp.target >> $UPGRADELOG 2>&1
    echo 50 | dialog --title "Upgrading OSP" --gauge "Upgrading Database" 10 70 0
-   python3 manage.py db init >> $UPGRADELOG
+   python3 manage.py db init >> $UPGRADELOG 2>&1
    echo 55 | dialog --title "Upgrading OSP" --gauge "Upgrading Database" 10 70 0
-   python3 manage.py db migrate >> $UPGRADELOG
+   python3 manage.py db migrate >> $UPGRADELOG 2>&1
    echo 65 | dialog --title "Upgrading OSP" --gauge "Upgrading Database" 10 70 0
-   python3 manage.py db upgrade >> $UPGRADELOG
+   python3 manage.py db upgrade >> $UPGRADELOG 2>&1
    echo 75 | dialog --title "Upgrading OSP" --gauge "Starting OSP" 10 70 0
-   systemctl start osp.target >> $UPGRADELOG
+   systemctl start osp.target >> $UPGRADELOG 2>&1
    echo 100 | dialog --title "Upgrading OSP" --gauge "Complete" 10 70 0
 }
 
@@ -56,34 +56,34 @@ install_osp() {
   if  $arch
   then
           echo "Installing for Arch" >> $installLog
-          sudo pacman -S python-pip base-devel unzip wget git redis gunicorn uwsgi-plugin-python libpq-dev ffmpeg --needed >> $installLog
+          sudo pacman -S python-pip base-devel unzip wget git redis gunicorn uwsgi-plugin-python libpq-dev ffmpeg --needed >> $installLog 2>&1
 
           sudo pip3 install -r requirements.txt
   else
-          echo "Installing for Debian - based" >> $installLog
+          echo "Installing for Debian - based" >> $installLog 2>&1
 
           # Get Dependancies
-          sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev unzip libpq-dev git -y >> $installLog
+          sudo apt-get install build-essential libpcre3 libpcre3-dev libssl-dev unzip libpq-dev git -y >> $installLog 2>&1
 
           # Setup Python
-          sudo apt-get install python3 python3-pip uwsgi-plugin-python -y >> $installLog
-          sudo pip3 install -r requirements.txt >> $installLog
+          sudo apt-get install python3 python3-pip uwsgi-plugin-python -y >> $installLog 2>&1
+          sudo pip3 install -r requirements.txt >> $installLog 2>&1
 
           # Install Redis
-          sudo apt-get install redis -y >> $installLog
+          sudo apt-get install redis -y >> $installLog 2>&1
   fi
   echo 10 | dialog --title "Installing OSP" --gauge "Configuring Redis" 10 70 0
-  sudo sed -i 's/appendfsync everysec/appendfsync no/' /etc/redis/redis.conf >> $installLog
-  sudo systemctl restart redis >> $installLog
+  sudo sed -i 's/appendfsync everysec/appendfsync no/' /etc/redis/redis.conf >> $installLog 2>&1
+  sudo systemctl restart redis >> $installLog 2>&1
 
 
   # Setup OSP Directory
   echo 20 | dialog --title "Installing OSP" --gauge "Setting up OSP Directory" 10 70 0
-  mkdir -p /opt/osp >> $installLog
+  mkdir -p /opt/osp >> $installLog 2>&1
   if cd ..
   then
-          sudo cp -rf -R * /opt/osp >> $installLog
-          sudo cp -rf -R .git /opt/osp >> $installLog
+          sudo cp -rf -R * /opt/osp >> $installLog 2>&1
+          sudo cp -rf -R .git /opt/osp >> $installLog 2>&1
   else
           echo "Unable to find installer directory. Aborting!" >> $installLog
           exit 1
@@ -98,21 +98,21 @@ install_osp() {
           sudo wget -q "http://www.zlib.net/zlib-1.2.11.tar.gz"
           sudo wget -q "https://bitbucket.org/nginx-goodies/nginx-sticky-module-ng/get/master.tar.gz"
           sudo tar xfz nginx-1.17.3.tar.gz
-          sudo unzip -qq -o v1.2.1.zip >> $installLog
+          sudo unzip -qq -o v1.2.1.zip >> $installLog 2>&1
           sudo tar xfz zlib-1.2.11.tar.gz
           sudo tar xfz master.tar.gz
           echo 30 | dialog --title "Installing OSP" --gauge "Building Nginx from Source" 10 70 0
           if cd nginx-1.17.3
           then
-                  ./configure --with-http_ssl_module --with-http_v2_module --with-http_auth_request_module --add-module=../nginx-rtmp-module-1.2.1 --add-module=../nginx-goodies-nginx-sticky-module-ng-08a395c66e42 --with-zlib=../zlib-1.2.11 --with-cc-opt="-Wimplicit-fallthrough=0" >> $installLog
+                  ./configure --with-http_ssl_module --with-http_v2_module --with-http_auth_request_module --add-module=../nginx-rtmp-module-1.2.1 --add-module=../nginx-goodies-nginx-sticky-module-ng-08a395c66e42 --with-zlib=../zlib-1.2.11 --with-cc-opt="-Wimplicit-fallthrough=0" >> $installLog 2>&1
                   echo 35 | dialog --title "Installing OSP" --gauge "Installing Nginx" 10 70 0
-                  sudo make install >> $installLog
+                  sudo make install >> $installLog 2>&1
           else
-                  echo "Unable to Build Nginx! Aborting." >> $installLog
+                  echo "Unable to Build Nginx! Aborting." >> $installLog 2>&1
                   exit 1
           fi
   else
-          echo "Unable to Download Nginx due to missing /tmp! Aborting." >> $installLog
+          echo "Unable to Download Nginx due to missing /tmp! Aborting." >> $installLog 2>&1
           exit 1
   fi
 
@@ -120,78 +120,78 @@ install_osp() {
   echo 40 | dialog --title "Installing OSP" --gauge "Copying Nginx Config Files" 10 70 0
   if cd $cwd/nginx
   then
-          sudo cp *.conf /usr/local/nginx/conf/ >> $installLog
+          sudo cp *.conf /usr/local/nginx/conf/ >> $installLog 2>&1
   else
-          echo "Unable to find downloaded Nginx config directory.  Aborting." >> $installLog
+          echo "Unable to find downloaded Nginx config directory.  Aborting." >> $installLog 2>&1
           exit 1
   fi
   # Enable SystemD
   echo 45 | dialog --title "Installing OSP" --gauge "Setting up Nginx SystemD" 10 70 0
   if cd $cwd/nginx
   then
-          sudo cp nginx-osp.service /etc/systemd/system/nginx-osp.service >> $installLog
-          sudo systemctl daemon-reload >> $installLog
-          sudo systemctl enable nginx-osp.service >> $installLog
+          sudo cp nginx-osp.service /etc/systemd/system/nginx-osp.service >> $installLog 2>&1
+          sudo systemctl daemon-reload >> $installLog 2>&1
+          sudo systemctl enable nginx-osp.service >> $installLog 2>&1
   else
-          echo "Unable to find downloaded Nginx config directory. Aborting." >> $installLog
+          echo "Unable to find downloaded Nginx config directory. Aborting." >> $installLog 2>&1
           exit 1
   fi
 
   echo 50 | dialog --title "Installing OSP" --gauge "Setting up Gunicorn SystemD" 10 70 0
   if cd $cwd/gunicorn
   then
-          sudo cp osp.target /etc/systemd/system/ >> $installLog
-          sudo cp osp-worker@.service /etc/systemd/system/ >> $installLog
-          sudo systemctl daemon-reload >> $installLog
-          sudo systemctl enable osp.target >> $installLog
+          sudo cp osp.target /etc/systemd/system/ >> $installLog 2>&1
+          sudo cp osp-worker@.service /etc/systemd/system/ >> $installLog 2>&1
+          sudo systemctl daemon-reload >> $installLog 2>&1
+          sudo systemctl enable osp.target >> $installLog 2>&1
   else
-          echo "Unable to find downloaded Gunicorn config directory. Aborting." >> $installLog
+          echo "Unable to find downloaded Gunicorn config directory. Aborting." >> $installLog 2>&1
           exit 1
   fi
 
   # Create HLS directory
   echo 60 | dialog --title "Installing OSP" --gauge "Creating OSP Video Directories" 10 70 0
-  sudo mkdir -p "$web_root" >> $installLog
-  sudo mkdir -p "$web_root/live" >> $installLog
-  sudo mkdir -p "$web_root/videos" >> $installLog
-  sudo mkdir -p "$web_root/live-rec" >> $installLog
-  sudo mkdir -p "$web_root/images" >> $installLog
-  sudo mkdir -p "$web_root/live-adapt" >> $installLog
-  sudo mkdir -p "$web_root/stream-thumb" >> $installLog
+  sudo mkdir -p "$web_root" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/live" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/videos" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/live-rec" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/images" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/live-adapt" >> $installLog 2>&1
+  sudo mkdir -p "$web_root/stream-thumb" >> $installLog 2>&1
 
   echo 70 | dialog --title "Installing OSP" --gauge "Setting Ownership of OSP Video Directories" 10 70 0
-  sudo chown -R "$http_user:$http_user" "$web_root" >> $installLog
+  sudo chown -R "$http_user:$http_user" "$web_root" >> $installLog 2>&1
 
-  sudo chown -R "$http_user:$http_user" /opt/osp >> $installLog
-  sudo chown -R "$http_user:$http_user" /opt/osp/.git >> $installLog
+  sudo chown -R "$http_user:$http_user" /opt/osp >> $installLog 2>&1
+  sudo chown -R "$http_user:$http_user" /opt/osp/.git >> $installLog 2>&1
 
   #Setup FFMPEG for recordings and Thumbnails
   echo 80 | dialog --title "Installing OSP" --gauge "Installing FFMPEG" 10 70 0
   if [ "$arch" = "false" ]
   then
-          sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y >> $installLog
-          sudo apt-get update >> $installLog
-          sudo apt-get install ffmpeg -y >> $installLog
+          sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y >> $installLog 2>&1
+          sudo apt-get update >> $installLog 2>&1
+          sudo apt-get install ffmpeg -y >> $installLog 2>&1
   fi
 
   # Setup Logrotate
   echo 90 | dialog --title "Installing OSP" --gauge "Setting Up Log Rotation" 10 70 0
   if cd /etc/logrotate.d
   then
-      sudo cp /opt/osp/setup/logrotate/* /etc/logrotate.d/ >> $installLog
+      sudo cp /opt/osp/setup/logrotate/* /etc/logrotate.d/ >> $installLog 2>&1
   else
-      sudo apt-get install logrorate >> $installLog
+      sudo apt-get install logrorate >> $installLog 2>&1
       if cd /etc/logrotate.d
       then
-          sudo cp /opt/osp/setup/logrotate/* /etc/logrotate.d/ >> $installLog
+          sudo cp /opt/osp/setup/logrotate/* /etc/logrotate.d/ >> $installLog 2>&1
       else
-          echo "Unable to setup logrotate" >> $installLog
+          echo "Unable to setup logrotate" >> $installLog 2>&1
       fi
   fi
   # Start Nginx
   echo 100 | dialog --title "Installing OSP" --gauge "Starting Nginx" 10 70 0
-  sudo systemctl start nginx-osp.service >> $installLog
-  sudo mv $installLog /opt/osp/logs >> /dev/null
+  sudo systemctl start nginx-osp.service >> $installLog 2>&1
+  sudo mv $installLog /opt/osp/logs >> /dev/null 2>&1
 }
 
 ##########################################################
@@ -238,8 +238,8 @@ if [ $# -eq 0 ]
           display_result "Install OSP"
           ;;
         2 )
-          systemctl restart nginx-osp
-          restartStatus=$(systemctl is-active nginx-osp)
+          systemctl restart nginx-osp > /dev/null 2>&1
+          restartStatus=$(systemctl is-active nginx-osp) > /dev/null 2>&1
           if [[ $restartStatus -eq Active ]]; then
             result=$(echo Nginx Restarted Successfully!)
           else
@@ -248,18 +248,18 @@ if [ $# -eq 0 ]
           display_result "Restart Nginx"
           ;;
         3 )
-          systemctl restart osp.target
-          worker5000=$(systemctl is-active osp-worker@5000)
-          worker5001=$(systemctl is-active osp-worker@5001)
-          worker5002=$(systemctl is-active osp-worker@5002)
-          worker5003=$(systemctl is-active osp-worker@5003)
-          worker5004=$(systemctl is-active osp-worker@5004)
-          worker5005=$(systemctl is-active osp-worker@5005)
-          worker5006=$(systemctl is-active osp-worker@5006)
-          worker5007=$(systemctl is-active osp-worker@5007)
-          worker5008=$(systemctl is-active osp-worker@5008)
-          worker5009=$(systemctl is-active osp-worker@5009)
-          worker5010=$(systemctl is-active osp-worker@5010)
+          systemctl restart osp.target > /dev/null 2>&1
+          worker5000=$(systemctl is-active osp-worker@5000) > /dev/null 2>&1
+          worker5001=$(systemctl is-active osp-worker@5001) > /dev/null 2>&1
+          worker5002=$(systemctl is-active osp-worker@5002) > /dev/null 2>&1
+          worker5003=$(systemctl is-active osp-worker@5003) > /dev/null 2>&1
+          worker5004=$(systemctl is-active osp-worker@5004) > /dev/null 2>&1
+          worker5005=$(systemctl is-active osp-worker@5005) > /dev/null 2>&1
+          worker5006=$(systemctl is-active osp-worker@5006) > /dev/null 2>&1
+          worker5007=$(systemctl is-active osp-worker@5007) > /dev/null 2>&1
+          worker5008=$(systemctl is-active osp-worker@5008) > /dev/null 2>&1
+          worker5009=$(systemctl is-active osp-worker@5009) > /dev/null 2>&1
+          worker5010=$(systemctl is-active osp-worker@50101) > /dev/null 2>&1
           result=$(echo "Worker 5000: $worker5000\nWorker 5001: $worker5001\nWorker 5002: $worker5002\nWorker 5003: $worker5003\nWorker 5004: $worker5004\nWorker 5005: $worker5005\nWorker 5006: $worker5006 \nWorker 5007: $worker5007 \nWorker 5008: $worker5008 \nWorker 5009: $worker5009\nWorker 5010: $worker5010")
           display_result "OSP Worker Status after Restart"
           ;;
@@ -268,11 +268,11 @@ if [ $# -eq 0 ]
           if [[ ! -d .git ]]; then
             result=$(echo "OSP not setup with Git.\n\n Please clone OSP Repo and try again")
           else
-            git fetch /dev/null > /dev/null
-            BRANCH=$(git rev-parse --abbrev-ref HEAD)
-            CURRENTCOMMIT=$(git rev-parse HEAD)
-            REMOTECOMMIT=$(git rev-parse origin/$BRANCH)
-            NEWVERSION=$(curl -s https://gitlab.com/Deamos/flask-nginx-rtmp-manager/-/raw/$BRANCH/version)
+            git fetch /dev/null > /dev/null 2>&1
+            BRANCH=$(git rev-parse --abbrev-ref HEAD) > /dev/null 2>&1
+            CURRENTCOMMIT=$(git rev-parse HEAD) > /dev/null 2>&1
+            REMOTECOMMIT=$(git rev-parse origin/$BRANCH) > /dev/null 2>&1
+            NEWVERSION=$(curl -s https://gitlab.com/Deamos/flask-nginx-rtmp-manager/-/raw/$BRANCH/version) > /dev/null 2>&1
             if [[ $CURRENTCOMMIT == $REMOTECOMMIT ]]; then
               result=$(echo "OSP is up-to-date on Branch $BRANCH")
             else
