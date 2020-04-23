f42322ee37c6024a740f51e84b1272f80d97d79a
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-23 19:58:41 -0400

    Modularization Part 4 Fixes

--- functions/templateFilters.py
+++ functions/templateFilters.py
@@ -2,7 +2,7 @@ from urllib.parse import urlparse
 import time
 import os
 
-from flask import current_app as app
+from flask import current_app
 
 from classes import Sec
 from classes import topics
@@ -10,15 +10,35 @@ from classes import topics
 from functions import votes
 from functions import commentsFunc
 
+def init(context):
+    context.jinja_env.filters['normalize_uuid'] = normalize_uuid
+    context.jinja_env.filters['normalize_urlroot'] = normalize_urlroot
+    context.jinja_env.filters['normalize_url'] = normalize_url
+    context.jinja_env.filters['normalize_date'] = normalize_date
+    context.jinja_env.filters['limit_title'] = limit_title
+    context.jinja_env.filters['format_kbps'] = format_kbps
+    context.jinja_env.filters['hms_format'] = hms_format
+    context.jinja_env.filters['get_topicName'] = get_topicName
+    context.jinja_env.filters['get_userName'] = get_userName
+    context.jinja_env.filters['get_Video_Upvotes'] = get_Video_Upvotes_Filter
+    context.jinja_env.filters['get_Stream_Upvotes'] = get_Stream_Upvotes_Filter
+    context.jinja_env.filters['get_Clip_Upvotes'] = get_Clip_Upvotes_Filter
+    context.jinja_env.filters['get_Video_Comments'] = get_Video_Comments_Filter
+    context.jinja_env.filters['get_pictureLocation'] = get_pictureLocation
+    context.jinja_env.filters['get_diskUsage'] = get_diskUsage
+    context.jinja_env.filters['testList'] = testList
+    context.jinja_env.filters['get_webhookTrigger'] = get_webhookTrigger
+    context.jinja_env.filters['get_hubStatus'] = get_hubStatus
+    context.jinja_env.filters['get_logType'] = get_logType
+
+
 #----------------------------------------------------------------------------#
 # Template Filters
 #----------------------------------------------------------------------------#
 
-@app.template_filter('normalize_uuid')
 def normalize_uuid(uuidstr):
     return uuidstr.replace("-", "")
 
-@app.template_filter('normalize_urlroot')
 def normalize_urlroot(urlString):
     parsedURLRoot = urlparse(urlString)
     URLProtocol = None
@@ -31,7 +51,6 @@ def normalize_urlroot(urlString):
     reparsedString = str(URLProtocol) + "://" + str(parsedURLRoot.hostname)
     return str(reparsedString)
 
-@app.template_filter('normalize_url')
 def normalize_url(urlString):
     parsedURL = urlparse(urlString)
     if parsedURL.port == 80:
@@ -43,23 +62,19 @@ def normalize_url(urlString):
     reparsedString = str(URLProtocol) + "://" + str(parsedURL.hostname) + str(parsedURL.path)
     return str(reparsedString)
 
-@app.template_filter('normalize_date')
 def normalize_date(dateStr):
     return str(dateStr)[:19]
 
-@app.template_filter('limit_title')
 def limit_title(titleStr):
     if len(titleStr) > 40:
         return titleStr[:37] + "..."
     else:
         return titleStr
 
-@app.template_filter('format_kbps')
 def format_kbps(bits):
     bits = int(bits)
     return round(bits/1000)
 
-@app.template_filter('hms_format')
 def hms_format(seconds):
     val = "Unknown"
     if seconds is not None:
@@ -67,15 +82,12 @@ def hms_format(seconds):
         val = time.strftime("%H:%M:%S", time.gmtime(seconds))
     return val
 
-@app.template_filter('get_topicName')
 def get_topicName(topicID):
     topicQuery = topics.topics.query.filter_by(id=int(topicID)).first()
     if topicQuery is None:
         return "None"
     return topicQuery.name
 
-
-@app.template_filter('get_userName')
 def get_userName(userID):
     userQuery = Sec.User.query.filter_by(id=int(userID)).first()
     if userQuery is None:
@@ -83,27 +95,22 @@ def get_userName(userID):
     else:
         return userQuery.username
 
-@app.template_filter('get_Video_Upvotes')
 def get_Video_Upvotes_Filter(videoID):
     result = votes.get_Video_Upvotes(videoID)
     return result
 
-@app.template_filter('get_Stream_Upvotes')
 def get_Stream_Upvotes_Filter(videoID):
     result = votes.get_Stream_Upvotes(videoID)
     return result
 
-@app.template_filter('get_Clip_Upvotes')
 def get_Clip_Upvotes_Filter(videoID):
     result = votes.get_Clip_Upvotes(videoID)
     return result
 
-@app.template_filter('get_Video_Comments')
 def get_Video_Comments_Filter(videoID):
     result = commentsFunc.get_Video_Comments(videoID)
     return result
 
-@app.template_filter('get_pictureLocation')
 def get_pictureLocation(userID):
     userQuery = Sec.User.query.filter_by(id=int(userID)).first()
     pictureLocation = None
@@ -114,27 +121,24 @@ def get_pictureLocation(userID):
 
     return pictureLocation
 
-@app.template_filter('get_diskUsage')
 def get_diskUsage(channelLocation):
+    with current_app.app_context:
+        videos_root = current_app.config['WEB_ROOT'] + 'videos/'
+        channelLocation = videos_root + channelLocation
 
-    videos_root = app.config['WEB_ROOT'] + 'videos/'
-    channelLocation = videos_root + channelLocation
+        total_size = 0
+        for dirpath, dirnames, filenames in os.walk(channelLocation):
+            for f in filenames:
+                fp = os.path.join(dirpath, f)
+                total_size += os.path.getsize(fp)
+        return "{:,}".format(total_size)
 
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(channelLocation):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return "{:,}".format(total_size)
-
-@ app.template_filter('testList')
 def testList(obj):
     if type(obj) == list:
         return True
     else:
         return False
 
-@app.template_filter('get_webhookTrigger')
 def get_webhookTrigger(webhookTrigger):
 
     webhookTrigger = str(webhookTrigger)
@@ -154,7 +158,6 @@ def get_webhookTrigger(webhookTrigger):
     }
     return webhookNames[webhookTrigger]
 
-@app.template_filter('get_hubStatus')
 def get_hubStatus(hubStatus):
 
     hubStatus = str(hubStatus)
@@ -173,7 +176,6 @@ def get_hubStatus(hubStatus):
 #        return hubQuery.serverAddress
 #    return "Unknown"
 
-@app.template_filter('get_logType')
 def get_logType(logType):
 
     logType = str(logType)
