534a607187d74dfadbf4db9d76c19ffec2a56a5b
David Lockwood <dave.r.lockwood@gmail.com>
2020-04-25 20:25:56 -0400

    Fix for Error Handlers

--- blueprints/errorhandler.py
+++ blueprints/errorhandler.py
@@ -7,13 +7,13 @@ from functions import system
 
 errorhandler_bp = Blueprint('errors', __name__)
 
-@errorhandler_bp.errorhandler(404)
+@errorhandler_bp.app_errorhandler(404)
 def page_not_found(e):
     sysSettings = settings.settings.query.first()
     system.newLog(0, "404 Error - " + str(request.url))
     return render_template(themes.checkOverride('404.html'), sysSetting=sysSettings), 404
 
-@errorhandler_bp.errorhandler(500)
+@errorhandler_bp.app_errorhandler(500)
 def page_not_found(e):
     sysSettings = settings.settings.query.first()
     system.newLog(0,"500 Error - " + str(request.url))
