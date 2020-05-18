e9d60c7b6324055e1624eaa28cab88bf26cfa7ac
David Lockwood <dave.r.lockwood@gmail.com>
2020-05-18 12:31:18 -0400

    Fixes and tests

--- setup/ejabberd/auth_osp.py
+++ setup/ejabberd/auth_osp.py
@@ -1,25 +1,9 @@
-#!/usr/bin/python
-
 ########################################################################
 #Setup
 ########################################################################
 
 import sys, logging, struct
 import requests
-
-sys.stderr = open('/var/log/ejabberd/extauth_err.log', 'a')
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s',
-                    filename='/var/log/ejabberd/extauth.log',
-                    filemode='a')
-
-logging.info('extauth script started, waiting for ejabberd requests')
-
-class EjabberdInputError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 def auth(user, host, password):
     payload = {'jid': user, 'host': host, 'token': password}
@@ -30,7 +14,6 @@ def auth(user, host, password):
         if code == 200:
             return True
         elif code == 400:
-            logging.debug("Wrong User of Password for user: %s@%s" % (user, host))
             return False
         else:
             return False
@@ -38,14 +21,11 @@ def auth(user, host, password):
         return False
 
 def read():
-    (pkt_size,) = struct.unpack('>H', sys.stdin.read(2))
+    (pkt_size,) = struct.unpack('>H', sys.stdin.buffer.read(2))
     pkt = sys.stdin.read(pkt_size)
     cmd = pkt.split(':')[0]
     if cmd == 'auth':
         u, s, p = pkt.split(':', 3)[1:]
-        logging.debug('user:' + u)
-        logging.debug('host:' + s)
-        logging.debug('pass:' + p)
         results = auth(u,s,p)
         write(results)
     else:
@@ -60,7 +40,4 @@ def write(result):
 
 if __name__ == "__main__":
     while True:
-        try:
-            read()
-        except Exception as e:
-            logging.debug(e)
+        read()
