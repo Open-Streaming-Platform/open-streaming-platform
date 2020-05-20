ae982fd2b960bd0e609201a0c563422eaaf58a9f
David Lockwood <dave.r.lockwood@gmail.com>
2020-05-20 12:27:37 -0400

    Fixes

--- static/js/chat.js
+++ static/js/chat.js
@@ -247,7 +247,7 @@ function parseOccupants(resp) {
       var userEntry = document.createElement('div');
       userEntry.className = "member my-2";
       //userEntry.innerHTML = '<img class="rounded shadow" src="https://picsum.photos/48"> ' + '<span>' + chatMembersArray['owner'][i]['username'] + '</span>';
-      userEntry.innerHTML = '<span>' + chatMembersArray['owner'][i]['username'] + '</span>';
+      userEntry.innerHTML = '<span>' + chatMembersArray['moderator'][i]['username'] + '</span>';
       document.getElementById('ModeratorList').appendChild(userEntry)
   }
 
