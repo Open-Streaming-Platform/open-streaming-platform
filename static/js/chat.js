89d2b90e7a74f0e03325ebe45812fdd745fc0236
David Lockwood <dave.r.lockwood@gmail.com>
2020-05-26 14:50:06 -0400

    Add XMPP Disco

--- static/js/chat.js
+++ static/js/chat.js
@@ -102,7 +102,7 @@ function onConnect(status) {
   } else if (status == Strophe.Status.CONNECTED) {
     console.log('Connected to XMPP Server.');
     fullJID = connection.jid; // full JID
-
+    connection.disco.info(fullJID);
     // set presence
     connection.send($pres());
     // set handlers
