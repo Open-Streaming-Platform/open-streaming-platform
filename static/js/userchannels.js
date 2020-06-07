90f060c2b1bcf34df4d1d34b8c1e7856fd61922a
David Lockwood <dave.r.lockwood@gmail.com>
2020-06-06 22:15:46 -0400

    Reverting last refactor

--- static/js/userchannels.js
+++ static/js/userchannels.js
@@ -13,17 +13,7 @@ var easymdeVideoClip = new EasyMDE({ autoDownloadFontAwesome: false, spellChecke
 var easymdeClipEditor = new EasyMDE({ autoDownloadFontAwesome: false, spellChecker: false, element: document.getElementById("clipEditDescription")});
 var newChanMDE = new EasyMDE({ autoDownloadFontAwesome: false, spellChecker: false, element: document.getElementById("description") });
 
-var chanDescriptionMDE = [];
-for (var i = 0; i < channelIDList; ++i) {
-    new EasyMDE({ autoDownloadFontAwesome: false, spellChecker: false, element: document.getElementById("description-" + channelIDList[i]) });
-}
-
 // Event Listeners
-for (var i = 0; i < channelIDList; ++i) {
-    document.getElementById('generateGUID-' + channelIDList[i]).addEventListener('click', function () {
-        document.getElementById('streamKey-' + channelIDList[i]).value = guid();
-    })
-}
 
 window.addEventListener("beforeunload", function (e) {
   socket.emit('cancelUpload', { data: videofilename });
