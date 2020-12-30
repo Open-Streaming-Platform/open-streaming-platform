fadc2d8fe4718e61c66c89bfc4147fdefec85f2b
Deamos <dave.r.lockwood@gmail.com>
2020-12-29 20:20:06 -0500

    Fixes

--- app.py
+++ app.py
@@ -419,30 +419,32 @@ def user_registered_sighandler(app, user, confirm_token, form_data=None):
 
 @app.before_request
 def do_before_request():
-
-    # Check all IP Requests for banned IP Addresses
-    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
-        requestIP = request.environ['REMOTE_ADDR']
-    else:
-        requestIP = request.environ['HTTP_X_FORWARDED_FOR']
-
-    banQuery = banList.ipList.query.filter_by(ipAddress=requestIP).first()
-    if banQuery != None:
-        return str({'error': 'banned', 'reason':banQuery.reason})
-
-    # Apply Guest UUID in Session and Handle Object
-    if current_user.is_authenticated is False:
-        if 'guestUUID' not in session:
-            session['guestUUID'] = str(uuid.uuid4())
-        GuestQuery = Sec.Guest.query.filter_by(UUID=session['guestUUID']).first()
-        if GuestQuery is not None:
-            GuestQuery.last_active_at = datetime.datetime.now()
-            GuestQuery.last_active_ip = requestIP
-            db.session.commit()
+    try:
+        # Check all IP Requests for banned IP Addresses
+        if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+            requestIP = request.environ['REMOTE_ADDR']
         else:
-            NewGuest = Sec.Guest(session['guestUUID'], requestIP)
-            db.session.add(NewGuest)
-            db.session.commit()
+            requestIP = request.environ['HTTP_X_FORWARDED_FOR']
+
+        banQuery = banList.ipList.query.filter_by(ipAddress=requestIP).first()
+        if banQuery != None:
+            return str({'error': 'banned', 'reason':banQuery.reason})
+
+        # Apply Guest UUID in Session and Handle Object
+        if current_user.is_authenticated is False:
+            if 'guestUUID' not in session:
+                session['guestUUID'] = str(uuid.uuid4())
+            GuestQuery = Sec.Guest.query.filter_by(UUID=session['guestUUID']).first()
+            if GuestQuery is not None:
+                GuestQuery.last_active_at = datetime.datetime.now()
+                GuestQuery.last_active_ip = requestIP
+                db.session.commit()
+            else:
+                NewGuest = Sec.Guest(session['guestUUID'], requestIP)
+                db.session.add(NewGuest)
+                db.session.commit()
+    except:
+        pass
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
