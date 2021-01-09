99fd6c2bd2c1ff3a6860ebab4010d8a362770162
Deamos <dave.r.lockwood@gmail.com>
2021-01-09 13:21:46 -0500

    Minor change

--- static/js/userchannels.js
+++ static/js/userchannels.js
@@ -366,7 +366,7 @@ socket.on('addMod', function (msg) {
     var mod = msg['mod'];
     var channelLoc = msg['channelLoc'];
     $('#mods-' + channelLoc).append('<div class="row" id="mod-' + channelLoc + '-' + mod + '"><div class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><p>' + mod + '</p></div><div class="col-xs-12 col-sm-12 col-md-12 col-lg-6"><button type="button" class="btn btn-sm btn-danger" onclick="deleteMod(\'' + mod + '\', ' + '\'' + channelLoc + '\')"><i class="far fa-trash-alt"></i></button></div></div>');
-    createNewBSAlert("Moderator permission granted to " + mod, "Success");
+    createNewBSAlert("Moderator permission granted to user", "Success");
 });
 
 socket.on('checkScreenShot', function (msg) {
