da2d8bf71e3eb8c005303219708f95132873472a
Deamos <dave.r.lockwood@gmail.com>
2021-02-06 14:35:33 -0500

    Added New User API Endpoint

--- blueprints/apis/user_ns.py
+++ blueprints/apis/user_ns.py
@@ -1,9 +1,23 @@
 from flask_restplus import Api, Resource, reqparse, Namespace
+from flask_security.utils import hash_password
+from flask import request
+import datetime
+import uuid
+import os
 
 from classes import Sec
 from classes.shared import db
 
+from app import user_datastore
+
+from functions import apiFunc
+
 api = Namespace('user', description='User Related Queries and Functions')
+
+newUserPost = reqparse.RequestParser()
+newUserPost.add_argument('username', type=str)
+newUserPost.add_argument('email', type=str)
+newUserPost.add_argument('password', type=str)
 
 @api.route('/<string:username>')
 @api.doc(params={'username': 'Username of OSP User'})
@@ -15,3 +29,33 @@ class api_1_ListUser(Resource):
         userQuery = Sec.User.query.filter_by(username=username).all()
         db.session.commit()
         return {'results': [ob.serialize() for ob in userQuery]}
+
+@api.route('/new')
+class api_1_CreateUser(Resource):
+    @api.doc(security='apikey')
+    @api.doc(responses={200: 'Success', 400: 'Request Error'})
+    def post(self):
+        """
+            **Admin API Key Required** - Create a New User
+        """
+        if 'X-API-KEY' in request.headers:
+            apiKey = request.headers['X-API-KEY']
+            adminKeyCheck = apiFunc.isValidAdminKey(apiKey)
+            if adminKeyCheck is True:
+                args = newUserPost.parse_args()
+                if 'username' in args and 'email' in args and 'password' in args:
+                    username = args['username']
+                    email = args['email']
+                    password = hash_password(args['password'])
+                    user_datastore.creatuser_datastore.create_user(email=email, username=username, password=password, active=True, confirmed_at=datetime.datetime.utcnow(), authType=0)
+                    defaultRoleQuery = Sec.Role.query.filter_by(default=True).all()
+                    newUserQuery = Sec.User.query.filter_by(email=email, username=username).first()
+                    for role in defaultRoleQuery:
+                        user_datastore.add_role_to_user(newUserQuery, role.name)
+                    newUserQuery.authType = 0
+                    newUserQuery.xmppToken = str(os.urandom(32).hex())
+                    newUserQuery.uuid = str(uuid.uuid4())
+                    db.session.commit()
+                    return {'results': newUserQuery.serialize()}
+
+        return {'results': {'message': "Request Error"}}, 400
