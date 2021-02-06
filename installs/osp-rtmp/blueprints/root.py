aa63379509cafaa576d542bcee64d556c69d6d65
Deamos <dave.r.lockwood@gmail.com>
2021-02-06 13:59:27 -0500

    Fixes

--- installs/osp-rtmp/blueprints/root.py
+++ installs/osp-rtmp/blueprints/root.py
@@ -1,7 +1,7 @@
 import requests
 from flask import Blueprint, request, url_for, render_template, redirect, current_app, send_from_directory, abort, flash
 
-from globals import globalvars
+from globals.globalvars import apiLocation
 
 root_bp = Blueprint('root', __name__)
 
@@ -12,8 +12,9 @@ def playback_auth_handler():
     username = request.form['username']
     secureHash = request.form['hash']
 
-    r = requests.post(globalvars.apiLocation+ '/apiv1/rtmp/playbackauth', data={'name': stream, 'addr': clientIP, 'username': username, 'hash': secureHash})
-    if r.status_code == 200:
+    r = requests.post(apiLocation + '/apiv1/rtmp/playbackauth', data={'name': stream, 'addr': clientIP, 'username': username, 'hash': secureHash})
+    results = r.json()
+    if results['results'] is True:
         return 'OK'
     else:
         return abort(400)
