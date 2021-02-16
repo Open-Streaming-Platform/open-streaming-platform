c5d5f0c37f6a9f68ee435d97fc4f99d17628897b
Deamos <dave.r.lockwood@gmail.com>
2021-02-15 21:48:03 -0500

    Fixes

--- static/js/chat.js
+++ static/js/chat.js
@@ -337,7 +337,7 @@ function process_stickers(msg) {
   while (result !== null) {
     var stickerName = result[1];
     var stickerData = stickerList.filter(d => d.name === stickerName);
-    if ((stickerData !== null) && (stickerData !== []) && (typeof(stickerData) !== "undefined")) {
+    if (stickerData !== []) {
         var stickerFilename = stickerData[0]['file']
         msg = msg.replace(`:${stickerName}:`, `<img src="${stickerFilename}" height="48px" alt=":${stickerName}:" title=":${stickerName}:" />`);
     } else {
