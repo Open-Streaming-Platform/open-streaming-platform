2884b13df61dc22fe7f7c2a385506b8484e5e6b1
Deamos <dave.r.lockwood@gmail.com>
2021-03-18 20:51:06 -0400

    Provide Method to Check for a Forced Destination

--- installs/osp-proxy/app.py
+++ installs/osp-proxy/app.py
@@ -15,18 +15,21 @@ rdis = redis.StrictRedis()
 #----------------------------------------------------------------------------#
 # Routes
 #----------------------------------------------------------------------------#
-
-location = {}
-
 @app.route('/<endpoint>/<channelLocation>/<file>')
 def home(endpoint,channelLocation,file):
-    if rdis.exists(channelLocation) == False:
-        header = {'X-Channel-ID': channelLocation}
-        r = requests.get(config.ospCoreAPI + '/rtmpCheck', headers=header)
-        rdis.set(channelLocation, r.headers['X_UpstreamHost'], 30)
-        return redirect('/' + r.headers['X_UpstreamHost'] + '/' + endpoint + '/' + channelLocation + '/' + file)
+
+    if hasattr(config, 'forceDestination'):
+        if config.forceDestinationType == "edge":
+            endpoint = endpoint.replace('live','edge')
+        return redirect('/' + forceDestination + '/' + endpoint + '/' + channelLocation + '/' + file)
     else:
-        return redirect('/' + str(rdis.get(channelLocation).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '/' + file)
+        if rdis.exists(channelLocation) == False:
+            header = {'X-Channel-ID': channelLocation}
+            r = requests.get(config.ospCoreAPI + '/rtmpCheck', headers=header)
+            rdis.set(channelLocation, r.headers['X_UpstreamHost'], 30)
+            return redirect('/' + r.headers['X_UpstreamHost'] + '/' + endpoint + '/' + channelLocation + '/' + file)
+        else:
+            return redirect('/' + str(rdis.get(channelLocation).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '/' + file)
 
 #----------------------------------------------------------------------------#
 # Launch.
