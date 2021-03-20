f2e8443cd7eb7330aceb09b7d4894b94243607d1
Deamos <dave.r.lockwood@gmail.com>
2021-03-20 19:05:05 -0400

    Adaptive Fixes

--- installs/osp-proxy/app.py
+++ installs/osp-proxy/app.py
@@ -34,6 +34,8 @@ def adaptive(endpoint,channelLocation):
 
 @app.route('/<endpoint>/<channelLocation>/<file>')
 def home(endpoint,channelLocation,file):
+    channelParsed = channelLocation.split('_','')[0]
+
     # Check if Force Destination Exists and Redirect to it, instead of querying OSP API
     if hasattr(config, 'forceDestination'):
         if config.forceDestinationType == "edge":
@@ -41,13 +43,13 @@ def home(endpoint,channelLocation,file):
         return redirect('/' + forceDestination + '/' + endpoint + '/' + channelLocation + '/' + file)
     else:
         # Check if Cached Redis RTMP Location Exists, If Not, Query API and Store the Result in Redis for a 30s Cache
-        if rdis.exists(channelLocation) == False:
-            header = {'X-Channel-ID': channelLocation}
+        if rdis.exists(channelParsed) == False:
+            header = {'X-Channel-ID': channelParsed}
             r = requests.get(config.ospCoreAPI + '/rtmpCheck', headers=header)
-            rdis.set(channelLocation, r.headers['X_UpstreamHost'], 30)
+            rdis.set(channelParsed, r.headers['X_UpstreamHost'], 30)
             return redirect('/' + r.headers['X_UpstreamHost'] + '/' + endpoint + '/' + channelLocation + '/' + file)
         else:
-            return redirect('/' + str(rdis.get(channelLocation).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '/' + file)
+            return redirect('/' + str(rdis.get(channelParsed).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '/' + file)
 
 #----------------------------------------------------------------------------#
 # Launch.
