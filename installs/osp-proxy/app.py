17fd09bb2e2bbec43e6195253649067a10eaa049
Deamos <dave.r.lockwood@gmail.com>
2021-03-20 20:08:38 -0400

    Add Check if API call returns an rtmp server

--- installs/osp-proxy/app.py
+++ installs/osp-proxy/app.py
@@ -27,7 +27,10 @@ def adaptive(endpoint,channelLocation):
         if rdis.exists(channelLocation) == False:
             header = {'X-Channel-ID': channelLocation}
             r = requests.get(config.ospCoreAPI + '/rtmpCheck', headers=header)
-            rdis.set(channelLocation, r.headers['X_UpstreamHost'], 30)
+            if 'X_UpstreamHost' not in r.headers:
+                abort(404)
+            else:
+                rdis.set(channelLocation, r.headers['X_UpstreamHost'], 30)
             return redirect('/' + r.headers['X_UpstreamHost'] + '/' + endpoint + '/' + channelLocation + '/' + file)
         else:
             return redirect('/' + str(rdis.get(channelLocation).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '.m3u8')
@@ -46,7 +49,10 @@ def home(endpoint,channelLocation,file):
         if rdis.exists(channelParsed) == False:
             header = {'X-Channel-ID': channelParsed}
             r = requests.get(config.ospCoreAPI + '/rtmpCheck', headers=header)
-            rdis.set(channelParsed, r.headers['X_UpstreamHost'], 30)
+            if 'X_UpstreamHost' not in r.headers:
+                abort(404)
+            else:
+                rdis.set(channelParsed, r.headers['X_UpstreamHost'], 30)
             return redirect('/' + r.headers['X_UpstreamHost'] + '/' + endpoint + '/' + channelLocation + '/' + file)
         else:
             return redirect('/' + str(rdis.get(channelParsed).decode("utf-8")) + '/' + endpoint + '/' + channelLocation + '/' + file)
