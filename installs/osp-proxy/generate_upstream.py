ed3c95978f46be12a65bc4b06b5cb40e246f3435
Deamos <dave.r.lockwood@gmail.com>
2021-03-21 19:35:03 -0400

    Resorted Order and Commented

--- installs/osp-proxy/generate_upstream.py
+++ installs/osp-proxy/generate_upstream.py
@@ -4,17 +4,7 @@ from jinja2 import Environment, FileSystemLoader
 
 from conf import config
 
-r = requests.get(config.ospCoreAPI + '/apiv1/server/rtmp')
-apiReturn = r.json()
-rtmpServerList = apiReturn['results']
-
-r = requests.get(config.ospCoreAPI + '/apiv1/server/')
-apiReturn = r.json()
-serverSettings = apiReturn['results']
-
-for entry in rtmpServerList:
-    entry['port'] = 5999
-
+# Check for a forced Destination
 if hasattr(config, 'forceDestination'):
     if not any(d['address'] == config.forceDestination for d in rtmpServerList):
         if hasattr(config, 'forceDestinationType'):
@@ -27,12 +17,28 @@ if hasattr(config, 'forceDestination'):
         forcedDestination = {'address': config.forceDestination, 'port': port}
         rtmpServerList.append(forcedDestination)
 
+# Pull Server Info for Protocol Data and Local RTMP Servers
+r = requests.get(config.ospCoreAPI + '/apiv1/server/')
+apiReturn = r.json()
+serverSettings = apiReturn['results']
+
+# Pull List of RTMP Servers
+r = requests.get(config.ospCoreAPI + '/apiv1/server/rtmp')
+apiReturn = r.json()
+rtmpServerList = apiReturn['results']
+
+# Sets the RTMP External Port for Files
+for entry in rtmpServerList:
+    entry['port'] = 5999
+
+# Verify there are no duplicate entries
 templateList = []
 for i in range(len(rtmpServerList)):
     if rtmpServerList[i] not in rtmpServerList[i + 1:]:
         templateList.append(rtmpServerList[i])
 rtmpServerList = templateList
 
+# Load Jinja2 Template Environment
 env = Environment(loader=FileSystemLoader('templates'))
 
 # Render rtmp-location.conf
