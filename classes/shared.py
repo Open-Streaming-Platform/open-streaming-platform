6ea2de82dff6b5fa7e55f1fd2fd4972393e709c5
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-09-16 18:55:28 -0400

    Revert "Testing Celery Tasks"
    This reverts commit 8b98b0e6de6b40d30e25485653cdd7bdaca8f198.

--- classes/shared.py
+++ classes/shared.py
@@ -5,7 +5,6 @@ from flask_mail import Mail
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_caching import Cache
-from celery import Celery
 
 db = SQLAlchemy()
 oauth = OAuth()
@@ -13,4 +12,3 @@ socketio = SocketIO()
 email = Mail()
 cache = Cache()
 limiter = Limiter(key_func=get_remote_address)
-celery = Celery()
