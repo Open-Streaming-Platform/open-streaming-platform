36a5305e75121a3234bcbadb04d27582832e4031
Deamos <deamosseraph@hotmail.com>
2021-09-23 14:14:57 -0400

    Simplifies Logic

--- functions/socketio/stream.py
+++ functions/socketio/stream.py
@@ -21,14 +21,9 @@ def handle_viewer_total_request(streamData, room=None):
     channelLoc = str(streamData['data'])
 
     viewers = xmpp.getChannelCounts(channelLoc)
-
-    #ChannelUpdateStatement = (update(Channel.Channel).where(Channel.Channel.channelLoc == channelLoc).values(currentViewers=viewers))
-    #channelQuery = Channel.Channel.query.filter_by(channelLoc=channelLoc).with_entities(Channel.Channel.id).first()
-    channelQuery = Channel.Channel.query.filter_by(channelLoc=channelLoc).first()
-    channelQuery.currentViewers = viewers
-    streamQuery = Stream.Stream.query.filter_by(linkedChannel=channelQuery.id).first()
-    streamQuery.currentViewers = viewers
-    #StreamUpdateStatement = (update(Stream.Stream).where(Stream.Stream.linkedChannel == channelQuery.id).values(currentViewers=viewers))
+    ChannelUpdateStatement = Channel.Channel.query.filter_by(channelLoc=channelLoc).update(dict(currentViewers=viewers))
+    channelQuery = Channel.Channel.query.filter_by(channelLoc=channelLoc).with_entities(Channel.Channel.id).first()
+    StreamUpdateStatement = Stream.Stream.query.filter_by(linkedChannel=channelQuery.id).update(dict(currentViewers=viewers))
 
     db.session.commit()
     db.session.close()
