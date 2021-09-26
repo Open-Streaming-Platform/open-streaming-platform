dbf86eaf9af9e67f267381569b42b56fafcd03e9
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-09-26 12:50:21 -0400

    Fixes

---
+++ static/js/adminv2.js
@@ -0,0 +1,44 @@
+// Admin Page Viewer Chart
+var ctx = document.getElementById('viewershipChart').getContext('2d');
+ctx.canvas.width = viewerChartWidth;
+ctx.canvas.height = viewerChartHeight;
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+        datasets: [
+            {
+            label: "Live Viewers",
+            fill: true,
+            borderColor: 'rgb(40, 90, 150)',
+            backgroundColor: 'rgb(40, 90, 150)',
+            spanGaps: true,
+            lineTension: 0,
+            data: viewerChartDataLive
+            },
+            {
+            label: "Video Viewers",
+            fill: true,
+            borderColor: 'rgb(90, 150, 40)',
+            backgroundColor: 'rgb(90, 150, 40)',
+            spanGaps: true,
+            lineTension: 0,
+            data:viewerChartDataVideo
+        }]
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    parser: 'YYYY-MM-DD',
+                    unit: 'day'
+                }
+            }],
+        }
+    }
+});
