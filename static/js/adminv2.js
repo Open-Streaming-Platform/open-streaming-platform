079d8da44d0cff3e88d7c760a50ff64a2357d2db
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-10-24 09:49:38 -0400

    Adding oAuth JS
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/adminv2.js
+++ static/js/adminv2.js
@@ -64,6 +64,57 @@ $('#maxClipLength').on('input', function() {
   }
 });
 
+// oAuth Related JS
+$(document).on("click", ".edit-oAuth-Button", function () {
+     var oAuthID = $(this).data('id');
+     var oAuthName = $(this).data('name');
+     var oAuthType = $(this).data('authtype');
+     var oAuthFriendlyName = $(this).data('friendlyname');
+     var oAuthDisplayColor = $(this).data('displaycolor');
+     var oAuthClientID = $(this).data('clientid');
+     var oAuthClientSecret = $(this).data('clientsecret');
+     var oAuthAccessTokenURL = $(this).data('accesstokenurl');
+     var oAuthAccessTokenParams = $(this).data('accesstokenparams');
+     var oAuthAuthorizeURL = $(this).data('authorizeurl');
+     var oAuthAuthorizeParams = $(this).data('authorizeparams');
+     var oAuthAPIBaseURL = $(this).data('apibaseurl');
+     var oAuthClientKwargs = $(this).data('clientkwargs');
+     var oAuthProfileEndpoint = $(this).data('profileendpoint');
+     var oAuthIDValue= $(this).data('idvalue');
+     var oAuthUsername = $(this).data('usernamevalue');
+     var oAuthEmail = $(this).data('emailvalue');
+     $("#oAuthID").val( oAuthID );
+     $("#oAuthPreset").val( oAuthType )
+     $("#oAuthName").val( oAuthName );
+     $("#oAuthFriendlyName").val( oAuthFriendlyName );
+     $("#oAuthColor").val( oAuthDisplayColor );
+     $("#oAuthClient_id").val( oAuthClientID );
+     $("#oAuthClient_secret").val( oAuthClientSecret );
+     $("#oAuthAccess_token_url").val( oAuthAccessTokenURL );
+     if (oAuthAccessTokenParams != 'None') {
+         $("#oAuthAccess_token_params").val(JSON.parse(oAuthAccessTokenParams));
+     }
+     $("#oAuthAuthorize_url").val( oAuthAuthorizeURL );
+     if (oAuthAuthorizeParams != 'None') {
+         $("#oAuthAuthorize_params").val(JSON.parse(oAuthAuthorizeParams));
+     }
+     $("#oAuthApi_base_url").val( oAuthAPIBaseURL );
+     if (oAuthClientKwargs != 'None') {
+         $("#oAuthClient_kwargs").val(JSON.parse(oAuthClientKwargs));
+     }
+     $("#oAuthProfile_endpoint").val( oAuthProfileEndpoint );
+     $("#oAuthIDValue").val( oAuthIDValue );
+     $("#oAuthUsername").val( oAuthUsername );
+     $("#oAuthEmail").val( oAuthEmail );
+     updateOAuthModalWindowLayout();
+     $("#newOauthModal").modal('show');
+});
+
+// Set Presets for oAuth
+$(document).on("change", "#oAuthPreset", function () {
+    updateOAuthModalWindowLayout();
+});
+
 // SocketIO Handlers
 socket.on('connect', function() {
     console.log('Connected to SocketIO');
@@ -117,4 +168,60 @@ function get_all_osp_component_status() {
     get_osp_component_status('osp_ejabberd_xmlrpc');
     get_osp_component_status('osp_database');
     get_osp_component_status('osp_redis');
+}
+
+function updateOAuthModalWindowLayout() {
+    var authType = document.getElementById("oAuthPreset").value;
+    switch(authType) {
+        case "Custom":
+            $("#oAuthAccess_token_url").attr("disabled", false);
+            $("#oAuthAccess_token_urlDiv").show();
+            $("#oAuthAccess_token_params").attr("disabled", false);
+            $("#oAuthAccess_token_paramsDiv").show();
+            $("#oAuthAuthorize_url").attr("disabled", false);
+            $("#oAuthAuthorize_urlDiv").show();
+            $("#oAuthAuthorize_params").attr("disabled", false);
+            $("#oAuthAuthorize_paramsDiv").show();
+            $("#oAuthApi_base_url").attr("disabled", false);
+            $("#oAuthApi_base_urlDiv").show();
+            $("#oAuthClient_kwargs").attr("disabled", false);
+            $("#oAuthClient_kwargsDiv").show();
+            $("#oAuthProfile_endpoint").attr("disabled", false);
+            $("#oAuthProfile_endpointDiv").show();
+            $("#oAuthIDValue").attr("disabled", false);
+            $("#oAuthIDValueDiv").show();
+            $("#oAuthUsername").attr("disabled", false);
+            $("#oAuthUsernameDiv").show();
+            $("#oAuthEmail").attr("disabled", false);
+            $("#oAuthEmailDiv").show();
+            break;
+        default:
+            $("#oAuthAccess_token_url").attr("disabled", true);
+            $("#oAuthAccess_token_urlDiv").hide();
+            $("#oAuthAccess_token_params").attr("disabled", true);
+            $("#oAuthAccess_token_paramsDiv").hide();
+            $("#oAuthAuthorize_url").attr("disabled", true);
+            $("#oAuthAuthorize_urlDiv").hide();
+            $("#oAuthAuthorize_params").attr("disabled", true);
+            $("#oAuthAuthorize_paramsDiv").hide();
+            $("#oAuthApi_base_url").attr("disabled", true);
+            $("#oAuthApi_base_urlDiv").hide();
+            $("#oAuthClient_kwargs").attr("disabled", true);
+            $("#oAuthClient_kwargsDiv").hide();
+            $("#oAuthProfile_endpoint").attr("disabled", true);
+            $("#oAuthProfile_endpointDiv").hide();
+            $("#oAuthIDValue").attr("disabled", true);
+            $("#oAuthIDValueDiv").hide();
+            $("#oAuthUsername").attr("disabled", true);
+            $("#oAuthUsernameDiv").hide();
+            $("#oAuthEmail").attr("disabled", true);
+            $("#oAuthEmailDiv").hide();
+            break;
+    }
+}
+
+function resetOAuthForm() {
+    document.getElementById("OAuthForm").reset();
+    $("#oAuthID").val('');
+    updateOAuthModalWindowLayout();
 }
