f99128260d402f58acb0f22ba017fd5f8a0c2530
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-10-31 19:18:10 -0400

    Fixes
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -23,28 +23,16 @@ socket.on('upvoteTotalResponse', function (msg) {
     }
 
     document.getElementById(upvoteDivID).innerHTML = msg['totalUpvotes'];
-    document.getElementById(upvoteDivID + 'Mobile').innerHTML = msg['totalUpvotes'];
 
     if (msg['myUpvote'] === 'True') {
         if (document.getElementById(upvoteIconID).classList.contains('far')) {
-        document.getElementById(upvoteIconID).classList.remove('far');
-        document.getElementById(upvoteIconID).classList.add('fas');
-    }
-    if (document.getElementById(upvoteButtonID).classList.contains('btn-outline-success')) {
-        document.getElementById(upvoteButtonID).classList.remove('btn-outline-success');
-        document.getElementById(upvoteButtonID).classList.add('btn-success');
-    }
-    if (msg['type'] === 'video') {
-        if (document.getElementById(upvoteIconID + 'Mobile').classList.contains('far')) {
-            document.getElementById(upvoteIconID + 'Mobile').classList.remove('far');
-            document.getElementById(upvoteIconID + 'Mobile').classList.add('fas');
+            document.getElementById(upvoteIconID).classList.remove('far');
+            document.getElementById(upvoteIconID).classList.add('fas');
         }
-
-        if (document.getElementById(upvoteButtonID + 'Mobile').classList.contains('btn-outline-success')) {
-            document.getElementById(upvoteButtonID + 'Mobile').classList.remove('btn-outline-success');
-            document.getElementById(upvoteButtonID + 'Mobile').classList.add('btn-success');
+        if (document.getElementById(upvoteButtonID).classList.contains('btn-outline-success')) {
+            document.getElementById(upvoteButtonID).classList.remove('btn-outline-success');
+            document.getElementById(upvoteButtonID).classList.add('btn-success');
         }
-    }
     } else if (msg['myUpvote'] === 'False') {
         if (document.getElementById(upvoteIconID).classList.contains('fas')) {
             document.getElementById(upvoteIconID).classList.remove('fas');
@@ -53,17 +41,6 @@ socket.on('upvoteTotalResponse', function (msg) {
         if (document.getElementById(upvoteButtonID).classList.contains('btn-success')) {
             document.getElementById(upvoteButtonID).classList.remove('btn-success');
             document.getElementById(upvoteButtonID).classList.add('btn-outline-success');
-        }
-
-        if (msg['type'] === 'video') {
-            if (document.getElementById(upvoteIconID + 'Mobile').classList.contains('fas')) {
-                document.getElementById(upvoteIconID + 'Mobile').classList.remove('fas');
-                document.getElementById(upvoteIconID + 'Mobile').classList.add('far');
-            }
-            if (document.getElementById(upvoteButtonID + 'Mobile').classList.contains('btn-success')) {
-                document.getElementById(upvoteButtonID + 'Mobile').classList.remove('btn-success');
-                document.getElementById(upvoteButtonID + 'Mobile').classList.add('btn-outline-success');
-            }
         }
     }
 });
