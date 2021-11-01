21188ca23b857db1c89b959ab8dae4886fb083b1
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-10-31 21:51:15 -0400

    Formatting Fix
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -49,10 +49,10 @@ socket.on('sendChanSubResults', function (msg) {
     var subButton = document.getElementById('chanSubStateButton');
     if (msg['state'] === true) {
         subButton.innerHTML = "<i class='fas fa-star'></i><span class='d-none d-sm-none d-md-inline'> Unsubscribe</span>";
-        subButton.className = "btn btn-sm btn-success";
+        subButton.className = "btn boxshadow btn-success";
     } else {
         subButton.innerHTML = "<i class='far fa-star'></i><span class='d-none d-sm-none d-md-inline'> Subscribe</span>";
-        subButton.className = "btn btn-sm btn-outline-success";
+        subButton.className = "btn boxshadow btn-outline-success";
     }
 });
 
