c4a2da17934ad102f6fc5503aff0f8a2cd78cea1
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-11-04 16:24:21 -0400

    Fixes
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- functions/videoFunc.py
+++ functions/videoFunc.py
@@ -215,7 +215,7 @@ def createClip(videoID, clipStart, clipStop, clipName, clipDescription):
                 os.mkdir(videos_root + recordedVidQuery.channel.channelLoc + '/clips')
 
             # FFMPEG Subprocess to Clip Video and generate Thumbnails
-            clipVideo = ffmpeg.input(videoLocation, ss=clipStart).trim(duration=newClipQuery.length).output(fullvideoLocation).run(quiet=True)
+            clipVideo = ffmpeg.input(videoLocation, ss=clipStart).output(fullvideoLocation, t=newClipQuery.length).run(quiet=True)
             #clipVideo = subprocess.call(['ffmpeg', '-ss', str(clipStart), '-i', videoLocation, '-t', str(newClipQuery.length), fullvideoLocation])
             #processResult = subprocess.call(['ffmpeg', '-ss', str(clipStart), '-i', videoLocation, '-s', '384x216', '-vframes', '1', fullthumbnailLocation])
             #gifprocessResult = subprocess.call(['ffmpeg', '-ss', str(clipStart), '-t', '3', '-i', videoLocation, '-filter_complex', '[0:v] fps=30,scale=w=384:h=-1,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1', '-y', fullgifLocation])
