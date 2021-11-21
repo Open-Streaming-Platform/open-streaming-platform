323525d9ea5ce863b4f75c5b01616274833dbaee
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-11-21 09:17:20 -0500

    Moving Scripts around
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/core.js
+++ static/js/core.js
@@ -23,3 +23,14 @@ function eraseCookie(name) {
 function toggleChannelSub(chanID) {
     socket.emit('toggleChannelSubscription', { channelID: chanID });
 }
+
+function guid() {
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+    }
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
