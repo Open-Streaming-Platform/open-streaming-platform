db5b0f1460201e862014fa2cfad7e6fa8f690c23
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-12-05 10:47:20 -0500

    Adding Inline Slash Command Test pt 3
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/chatv2.js
+++ static/js/chatv2.js
@@ -422,9 +422,9 @@ function onMessage(msg) {
           if (msg.charAt(0) === '/') {
               var commandString = msg.substring(1);
               var splitCommandString = commandString.split(' ');
-              var userJID = from.split('@')[0];
+              var channelId = from.split('@')[0];
 
-              var commandArray = {command: splitCommandString[0], argString: splitCommandString[1], JID: userJID, xmppAuth: xmppPassword};
+              var commandArray = {command: splitCommandString[0], argString: splitCommandString[1], channelId: channelId, userUUID: userUUID, xmppAuth: xmppPassword};
               console.log(commandArray);
           } else {
               var tempNode = document.querySelector("div[data-type='chatmessagetemplate']").cloneNode(true);
