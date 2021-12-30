41ef7776bca98cbc2f2e9bcf3130fcf9bc5857d0
Dave Lockwood <dave.r.lockwood@gmail.com>
2021-12-30 11:42:02 -0500

    Change for unneeded check as sqlite no longer supported
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- app.py
+++ app.py
@@ -95,14 +95,11 @@ app.jinja_env.cache = {}
 app.config['WEB_ROOT'] = globalvars.videoRoot
 app.config['SQLALCHEMY_DATABASE_URI'] = config.dbLocation
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-if config.dbLocation[:6] != "sqlite":
-    app.config['SQLALCHEMY_MAX_OVERFLOW'] = -1
-    app.config['SQLALCHEMY_POOL_RECYCLE'] = 300
-    app.config['SQLALCHEMY_POOL_TIMEOUT'] = 600
-    app.config['MYSQL_DATABASE_CHARSET'] = "utf8"
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'encoding': 'utf8', 'pool_use_lifo': 'False', 'pool_size': 10, "pool_pre_ping": True}
-else:
-    pass
+app.config['SQLALCHEMY_MAX_OVERFLOW'] = -1
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 300
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 600
+app.config['MYSQL_DATABASE_CHARSET'] = "utf8"
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'encoding': 'utf8', 'pool_use_lifo': 'False', 'pool_size': 10, "pool_pre_ping": True}
 app.config['SESSION_TYPE'] = 'redis'
 app.config['SESSION_COOKIE_SAMESITE'] = "Lax"
 app.config['SESSION_COOKIE_NAME'] = 'ospSession'
