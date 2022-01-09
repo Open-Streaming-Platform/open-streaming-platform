46ec6bd61e654203af1b19a55760fc82b53c8ed0
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-01-09 11:27:16 -0500

    Add Base Notification Link and Logic
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

---
+++ static/js/notifications.js
@@ -0,0 +1,23 @@
+function clearListNotification(notificationID) {
+    var listEntry = document.getElementById('notificationList-' + notificationID);
+    listEntry.parentNode.removeChild(listEntry);
+    clearNotification(notificationID);
+    if (notificationCount <= 0) {
+        emptyNotificationList.style.display = "block";
+    }
+}
+
+function clearAllListNotifications() {
+  var ids = $('.notificationBodyFull .notification-box').map(function(){
+    return $(this).attr('id');
+    }).get();
+  ids.forEach(function (item, index) {
+    if (item != 'notificationList-empty') {
+      item = item.replace('notificationList-','');
+      clearNotification(item);
+    }
+  });
+  if (notificationCount <= 0) {
+    emptyNotificationList.style.display = "block";
+  }
+}
