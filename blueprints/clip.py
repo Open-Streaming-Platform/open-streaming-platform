ddd355a3e663634ea19260e14e9aa390048621f7
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-01-22 11:59:18 -0500

    Formatting
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- blueprints/clip.py
+++ blueprints/clip.py
@@ -72,7 +72,7 @@ def view_clip_page(clipID):
 
             if isEmbedded is None or isEmbedded == "False":
 
-                randomClips = RecordedVideo.Clips.query.filter(RecordedVideo.Clips.id != clipQuery.id).filter(RecordedVideo.Clips.published == True).order_by(func.random()).limit(12)
+                randomClips = RecordedVideo.Clips.query.filter(RecordedVideo.Clips.id != clipQuery.id).filter(RecordedVideo.Clips.published == True).order_by(func.random()).limit(4)
 
                 subState = False
                 if current_user.is_authenticated:
