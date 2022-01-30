67ca0dfb16e187cb0b1cba19c33335f564d34db6
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-01-30 11:45:55 -0500

    Add Link for Show More Fix
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- functions/cachedDbCalls.py
+++ functions/cachedDbCalls.py
@@ -30,7 +30,7 @@ def searchStreams(term):
     if term is not None:
         StreamNameQuery = Stream.Stream.query.filter(Stream.Stream.active == True, Stream.Stream.streamName.like("%" + term + "%"))\
             .join(Channel.Channel, Channel.Channel.id == Stream.Stream.linkedChannel)\
-            .with_entities(Stream.Stream.id, Stream.Stream.streamName, Channel.Channel.channelLoc, Stream.Stream.uuid, Stream.Stream.startTimestamp,
+            .with_entities(Stream.Stream.id, Stream.Stream.streamName, Channel.Channel.channelLoc, Stream.Stream.uuid,
                            Stream.Stream.topic, Stream.Stream.currentViewers, Stream.Stream.totalViewers, Stream.Stream.active, Stream.Stream.rtmpServer).all()
 
         resultsArray = StreamNameQuery
