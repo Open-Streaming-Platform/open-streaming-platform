a8ebab7a9fd81013c7e29ac97cf15733029df569
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-01-30 22:17:43 -0500

    Add Placeholder for Terms and Privacy per Issue #278 Fix
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- classes/settings.py
+++ classes/settings.py
@@ -35,8 +35,8 @@ class settings(db.Model):
     hubUUID = db.Column(db.String(255))
     hubURL = db.Column(db.String(255))
     maxVideoRetention = db.Column(db.Integer)
-    terms = db.Column(db.Text)
-    privacy = db.Column(db.Text)
+    terms = db.Column(db.String(4096))
+    privacy = db.Column(db.String(4096))
 
     def __init__(self, siteName, siteProtocol, siteAddress, smtpAddress, smtpPort, smtpTLS, smtpSSL, smtpUsername, smtpPassword, smtpSendAs, allowRecording, allowUploads, adaptiveStreaming, showEmptyTables, allowComments, version):
         self.siteName = siteName
