4921a2095eaed737c8de5bfd015d3a843cc272d9
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-03-27 09:48:58 -0400

    Add Delete Comment Modal and JS logic
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- static/js/videoplayer.js
+++ static/js/videoplayer.js
@@ -168,3 +168,17 @@ function hideComments() {
     commentsDiv.style.display = 'none';
     contentsDiv.className = 'col-9 mx-auto';
 }
+
+function confirmDeleteComment(commentId) {
+    document.getElementById('deleteCommentId').value = commentId;
+    openModal('confirmDeleteCommentModal');
+}
+
+function deleteComment(){
+    var commentId = document.getElementById('deleteCommentId').value;
+    document.getElementById('deleteCommentId').value = '';
+    socket.emit('deleteComment', {commentID: commentId});
+    var commentDiv = document.getElementById('vidComment-' + commentId);
+    commentDiv.parentElement.removeChild(commentDiv);
+}
+
