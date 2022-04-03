0f616e77705cfaeabdc4c42b4a548d2ddd5dd426
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-04-03 14:10:32 -0400

    Adds functionality for user to delete own account with 48 hour bake time
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- functions/scheduled_tasks/scheduler.py
+++ functions/scheduled_tasks/scheduler.py
@@ -2,7 +2,7 @@ import logging
 from celery.schedules import crontab
 from classes.shared import celery
 
-from functions.scheduled_tasks import video_tasks, message_tasks
+from functions.scheduled_tasks import video_tasks, message_tasks, security_tasks
 
 from datetime import timedelta
 
@@ -15,7 +15,7 @@ def setup_periodic_tasks(sender, **kwargs):
     """
     video_tasks.setup_video_tasks(sender, **kwargs)
     message_tasks.setup_message_tasks(sender, **kwargs)
-    message_tasks.setup_security_tasks(sender, **kwargs)
+    security_tasks.setup_security_tasks(sender, **kwargs)
 
 
 
