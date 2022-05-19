c1bbf071acea223f495f5494b9cecc163660b485
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-05-19 09:25:46 -0400

    Variablize build directory

--- build-docker-directory.sh
+++ build-docker-directory.sh
@@ -1,44 +1,46 @@
+BUILDDIR="docker-build"
+
 # Create Docker Build Directory
-mkdir -p docker-build
-mkdir -p docker-build/osp-core
-mkdir -p docker-build/osp-rtmp
-mkdir -p docker-build/osp-proxy
-mkdir -p docker-build/osp-edge
-mkdir -p docker-build/osp-ejabberd
+mkdir -p $BUILDDIR
+mkdir -p $BUILDDIR/osp-core
+mkdir -p $BUILDDIR/osp-rtmp
+mkdir -p $BUILDDIR/osp-proxy
+mkdir -p $BUILDDIR/osp-edge
+mkdir -p $BUILDDIR/osp-ejabberd
 
-# Build OSP-Core docker-build Directory
-mkdir -p docker-build/osp-core/installs
+# Build OSP-Core $BUILDDIR Directory
+mkdir -p $BUILDDIR/osp-core/installs
 
-cp -R installs/nginx-core docker-build/osp-core/installs
-cp -R blueprints docker-build/osp-core
-cp -R classes docker-build/osp-core
-cp -R conf docker-build/osp-core
-cp -R functions docker-build/osp-core
-cp -R globals docker-build/osp-core
-cp -R logs docker-build/osp-core
-cp -R setup docker-build/osp-core
-cp -R static docker-build/osp-core
-cp -R templates docker-build/osp-core
+cp -R installs/nginx-core $BUILDDIR/osp-core/installs
+cp -R blueprints $BUILDDIR/osp-core
+cp -R classes $BUILDDIR/osp-core
+cp -R conf $BUILDDIR/osp-core
+cp -R functions $BUILDDIR/osp-core
+cp -R globals $BUILDDIR/osp-core
+cp -R logs $BUILDDIR/osp-core
+cp -R setup $BUILDDIR/osp-core
+cp -R static $BUILDDIR/osp-core
+cp -R templates $BUILDDIR/osp-core
 
-cp __init__.py docker-build/osp-core
-cp app.py docker-build/osp-core
-cp manage.py docker-build/osp-core
-cp installs/docker/OSP-Core/Dockerfile docker-build/osp-core
-cp -R installs/docker/OSP-Core/docker-files.d docker-build/osp-core
+cp __init__.py $BUILDDIR/osp-core
+cp app.py $BUILDDIR/osp-core
+cp manage.py $BUILDDIR/osp-core
+cp installs/docker/OSP-Core/Dockerfile $BUILDDIR/osp-core
+cp -R installs/docker/OSP-Core/docker-files.d $BUILDDIR/osp-core
 
-# Build OSP-RTMP docker-build Directory
-mkdir -p docker-build/osp-rtmp/installs
-cp -R installs/nginx-core docker-build/osp-rtmp/installs
-cp -R installs/osp-rtmp/* docker-build/osp-rtmp/
-cp -R setup docker-build/osp-rtmp
+# Build OSP-RTMP $BUILDDIR Directory
+mkdir -p $BUILDDIR/osp-rtmp/installs
+cp -R installs/nginx-core $BUILDDIR/osp-rtmp/installs
+cp -R installs/osp-rtmp/* $BUILDDIR/osp-rtmp/
+cp -R setup $BUILDDIR/osp-rtmp
 
-cp installs/docker/OSP-RTMP/Dockerfile docker-build/osp-rtmp
-cp -R installs/docker/OSP-RTMP/docker-files.d docker-build/osp-rtmp
+cp installs/docker/OSP-RTMP/Dockerfile $BUILDDIR/osp-rtmp
+cp -R installs/docker/OSP-RTMP/docker-files.d $BUILDDIR/osp-rtmp
 
-# Build OSP-Ejabberd docker-build Directory
-mkdir -p docker-build/osp-ejabberd/installs
-cp -R installs/ejabberd docker-build/osp-ejabberd/installs
-cp -R installs/nginx-core docker-build/osp-ejabberd/installs
+# Build OSP-Ejabberd $BUILDDIR Directory
+mkdir -p $BUILDDIR/osp-ejabberd/installs
+cp -R installs/ejabberd $BUILDDIR/osp-ejabberd/installs
+cp -R installs/nginx-core $BUILDDIR/osp-ejabberd/installs
 
-cp installs/docker/Ejabberd/Dockerfile docker-build/osp-ejabberd
-cp -R installs/docker/Ejabberd/docker-files.d docker-build/osp-ejabberd
+cp installs/docker/Ejabberd/Dockerfile $BUILDDIR/osp-ejabberd
+cp -R installs/docker/Ejabberd/docker-files.d $BUILDDIR/osp-ejabberd
