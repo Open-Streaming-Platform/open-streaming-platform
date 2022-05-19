dc5b534fe31675d993ecab556f5575f595577f07
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-05-19 07:38:01 -0400

    Future Docker Tweaks

--- build-docker-directory.sh
+++ build-docker-directory.sh
@@ -22,14 +22,22 @@ cp -R templates docker-build/osp-core
 cp __init__.py docker-build/osp-core
 cp app.py docker-build/osp-core
 cp manage.py docker-build/osp-core
+cp installs/docker/OSP-Core/Dockerfile docker-build/osp-core
+cp -R installs/docker/OSP-Core/docker-files.d docker-build/osp-core
 
 # Build OSP-RTMP docker-build Directory
 mkdir -p docker-build/osp-rtmp/installs
 cp -R installs/nginx-core docker-build/osp-rtmp/installs
-cp -R installs/osp-rtmp docker-build/osp-rtmp/installs
+cp -R installs/osp-rtmp/* docker-build/osp-rtmp/
 cp -R setup docker-build/osp-rtmp
+
+cp installs/docker/OSP-RTMP/Dockerfile docker-build/osp-rtmp
+cp -R installs/docker/OSP-RTMP/docker-files.d docker-build/osp-rtmp
 
 # Build OSP-Ejabberd docker-build Directory
 mkdir -p docker-build/osp-ejabberd/installs
 cp -R installs/ejabberd docker-build/osp-ejabberd/installs
 cp -R installs/nginx-core docker-build/osp-ejabberd/installs
+
+cp installs/docker/Ejabberd/Dockerfile docker-build/osp-ejabberd
+cp -R installs/docker/Ejabberd/docker-files.d docker-build/osp-ejabberd
