7237f5acf509c7f109f93d43f49413e593245256
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-05-22 19:21:35 -0400

    Add 0.9.0.sh upgrade script fixes
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- setup/upgrade/0.9.0.sh
+++ setup/upgrade/0.9.0.sh
@@ -1,15 +1,16 @@
-mysql --database="osp" --execute="DROP TABLE alembic_version;"
-mkdir /tmp/osp
+sudo mysql --database="osp" --execute="DROP TABLE alembic_version;"
+sudo mkdir /tmp/osp
 sudo mv /opt/osp/migrations/versions/* /tmp/osp
 sudo pip3 install -r /opt/osp/setup/requirements.txt
-python3 manage.py db migrate
-python3 manage.py db upgrade
-mysql --database="osp" --execute="DROP TABLE alembic_version;"
+sudo python3 manage.py db migrate
+sudo python3 manage.py db upgrade
+sudo mysql --database="osp" --execute="DROP TABLE alembic_version;"
 sudo rm -rf /opt/osp/migrations/versions/*
 sudo mv /tmp/osp/* /opt/osp/migrations/versions
-python3 manage.py db stamp head
-mysql --database="osp" --execute="update settings set systemTheme='Defaultv3';"
+sudo python3 manage.py db stamp head
+sudo mysql --database="osp" --execute="update settings set systemTheme='Defaultv3';"
 sudo bash osp-config.sh reset nginx
 sudo bash osp-config.sh upgrade osp
+sudo chown -R www-data:www-data /opt/osp
 clear
 echo "OSP 0.9.x Upgrade Complete!"
