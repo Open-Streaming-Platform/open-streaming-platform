ff095674cc1fa3d6fb191da1920df5261a024ab8
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-06-21 19:17:22 -0400

    Removing Git Pull from osp-config.sh to prevent future issue
    Signed-off-by: Dave Lockwood <dave.r.lockwood@gmail.com>

--- osp-config.sh
+++ osp-config.sh
@@ -626,9 +626,7 @@ upgrade_osp() {
    if cd /opt/osp
    then
      echo 0 | dialog --title "Upgrading OSP" --gauge "Pulling Git Repo" 10 70 0
-     sudo git stash >> $UPGRADELOG 2>&1
-     sudo git pull >> $UPGRADELOG 2>&1
-     echo 15 | dialog --title "Upgrading OSP" --gauge "Setting /opt/osp Ownership" 10 70 0
+     echo 10 | dialog --title "Upgrading OSP" --gauge "Setting /opt/osp Ownership" 10 70 0
      sudo chown -R $http_user:$http_user /opt/osp >> $UPGRADELOG 2>&1
      echo 25 | dialog --title "Upgrading OSP" --gauge "Stopping OSP" 10 70 0
      sudo systemctl stop osp.target >> $UPGRADELOG 2>&1
@@ -655,7 +653,6 @@ upgrade_osp() {
 }
 
 upgrade_proxy() {
-  sudo git pull >> $OSPLOG 2>&1
   sudo pip3 install -r $DIR/installs/osp-proxy/setup/requirements.txt >> $OSPLOG 2>&1
   sudo cp $DIR/installs/osp-proxy/setup/nginx/locations/*.conf /usr/local/nginx/conf/locations >> $OSPLOG 2>&1
   sudo cp $DIR/installs/osp-proxy/setup/nginx/servers/*.conf /usr/local/nginx/conf/servers >> $OSPLOG 2>&1
@@ -664,7 +661,6 @@ upgrade_proxy() {
 }
 
 upgrade_rtmp() {
-  sudo git pull >> $OSPLOG 2>&1
   sudo pip3 uninstall -r $DIR/installs/osp-rtmp/setup/remove_requirements.txt -y >> $OSPLOG 2>&1
   sudo pip3 install -r $DIR/installs/osp-rtmp/setup/requirements.txt >> $OSPLOG 2>&1
   sudo cp -rf $DIR/installs/osp-rtmp/setup/nginx/servers/*.conf /usr/local/nginx/conf/servers >> $OSPLOG 2>&1
@@ -673,7 +669,6 @@ upgrade_rtmp() {
 }
 
 upgrade_ejabberd() {
-  sudo git pull >> $OSPLOG 2>&1
   sudo cp -rf $DIR/installs/ejabberd/setup/auth_osp.py /usr/local/ejabberd/conf/auth_osp.py >> $OSPLOG 2>&1
   sudo cp -rf $DIR/installs/ejabberd/setup/nginx/locations/ejabberd.conf /usr/local/nginx/conf/locations/ >> $OSPLOG 2>&1
 }
