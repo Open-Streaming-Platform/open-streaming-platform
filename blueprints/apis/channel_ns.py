558fe73859e9565b6f826b01e5e202f28dd02a27
Dave Lockwood <dave.r.lockwood@gmail.com>
2022-12-25 18:06:38 -0500

    More optimizations on RTMP server class calls

--- blueprints/apis/channel_ns.py
+++ blueprints/apis/channel_ns.py
@@ -60,7 +60,12 @@ def checkRTMPAuthIP(requestData):
     else:
         requestIP = requestData.environ["HTTP_X_FORWARDED_FOR"]
 
-    authorizedRTMPServers = settings.rtmpServer.query.all()
+    authorizedRTMPServers = settings.rtmpServer.query.with_entities(
+        settings.rtmpServer.id,
+        settings.rtmpServer.hide,
+        settings.rtmpServer.active,
+        settings.rtmpServer.address,
+    ).all()
 
     receivedIP = requestIP
     ipList = requestIP.split(",")
