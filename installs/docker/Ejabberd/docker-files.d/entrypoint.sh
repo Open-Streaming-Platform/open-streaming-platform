fe2da4a436569a1c3a3bad9abb0ea0eeedb4247a
Devedse <davydavidse@gmail.com>
2023-01-21 16:42:20 +0100

    Updated to latest version of ejabberd

--- installs/docker/Ejabberd/docker-files.d/entrypoint.sh
+++ installs/docker/Ejabberd/docker-files.d/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-cp -u -p /home/ejabberd/run/ejabberd.yml /home/ejabberd/conf/ejabberd.yml
-cp -u -p /home/ejabberd/run/auth_osp.py /home/ejabberd/conf/auth_osp.py
+cp -u -p /opt/ejabberd/run/ejabberd.yml /opt/ejabberd/conf/ejabberd.yml
+cp -u -p /opt/ejabberd/run/auth_osp.py /opt/ejabberd/conf/auth_osp.py
 
 # Configure ejabberd
 export EJABBERD_DOMAIN
-sed -i "s/CHANGEME/$EJABBERD_DOMAIN/g" /home/ejabberd/conf/ejabberd.yml
+sed -i "s/CHANGEME/$EJABBERD_DOMAIN/g" /opt/ejabberd/conf/ejabberd.yml
 
 #export EJABBERD_XMLRPC_ALLOWIP
 #IFS="," read -a XMLRPCARRAY <<< $EJABBERD_XMLRPC_ALLOWIP
@@ -13,12 +13,12 @@ sed -i "s/CHANGEME/$EJABBERD_DOMAIN/g" /home/ejabberd/conf/ejabberd.yml
 #do
 #      XMLRPCSTRING+="      - $i\n"
 #done
-#sed -i "s/ALLOWXMLRPC/$XMLRPCSTRING/g" /home/ejabberd/conf/ejabberd.yml
+#sed -i "s/ALLOWXMLRPC/$XMLRPCSTRING/g" /opt/ejabberd/conf/ejabberd.yml
 
 export OSP_API_PROTOCOL
 export OSP_API_DOMAIN
 export EJABBERD_PASSWORD
 
-chown -R ejabberd:ejabberd /home/ejabberd
+chown -R ejabberd:ejabberd /opt/ejabberd
 
 supervisord --nodaemon --configuration /run/supervisord.conf
