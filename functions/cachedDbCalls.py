8dfcca4eae96d60e0b6d6d4535aefa7c6b972076
Dave Lockwood <dave.r.lockwood@gmail.com>
2023-03-12 15:02:16 -0400

    Additional cleanup

--- functions/cachedDbCalls.py
+++ functions/cachedDbCalls.py
@@ -51,19 +51,7 @@ def getChannelLiveViewsByDate(channelId):
 
 @cache.memoize(timeout=600)
 def getVideoViewsByDate(videoId):
-    videoViewCountQuery = (
-        db.session.query(
-            func.date(views.views.date), func.count(views.views.id)
-        )
-        .filter(views.views.viewType == 1)
-        .filter(views.views.itemID == videoId)
-        .filter(
-            views.views.date
-            > (datetime.datetime.utcnow() - datetime.timedelta(days=30))
-        )
-        .group_by(func.date(views.views.date))
-        .all()
-    )
+    videoViewCountQuery = views.views.query.filter_by(viewType=1, itemId=videoId).filter(views.views.date > (datetime.datetime.utcnow() - datetime.timedelta(days=30))).with_entities(func.date(views.views.date), func.count(views.views.id)).group_by(func.date(views.views.date)).all()
     return videoViewCountQuery
 
 # Stream Related DB Calls
