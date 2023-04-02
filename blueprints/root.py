7658aba470e84c558961eb6347ee461bc5c3cda1
Dave Lockwood <dave.r.lockwood@gmail.com>
2023-04-01 21:01:14 -0400

    Fix to XMPP Post method

--- blueprints/root.py
+++ blueprints/root.py
@@ -672,7 +672,7 @@ def xmpp_proxy():
         response = Response(resp.content, resp.status_code, headers)
         return response
     elif request.method=="POST":
-        resp = requests.post(xmppConnector, json=request.get_json())
+        resp = requests.post(xmppConnector, data=request.get_data())
         excluded_headers = ["content-encoding", "content-length", "transfer-encoding", "connection"]
         headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
         response = Response(resp.content, resp.status_code, headers)
