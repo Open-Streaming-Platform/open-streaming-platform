ec47918c877f7cbed90b4377c81c12bfe1caa592
Deamos <dave.r.lockwood@gmail.com>
2023-04-17 13:57:07 -0400

    Adds endpoint dedicated to only live hub channels

--- blueprints/apis/channel_ns.py
+++ blueprints/apis/channel_ns.py
@@ -293,7 +293,7 @@ class api_1_ActiveChannels(Resource):
         sysSettings = cachedDbCalls.getSystemSettings()
         activeChannels = cachedDbCalls.getLiveChannels()
         return  {"results": activeChannels }, 200
-    
+
 @api.route("/hubChannels")
 class api_1_hubChannels(Resource):
     @api.doc(responses={200: "Success", 400: "Request Error"})
@@ -304,6 +304,17 @@ class api_1_hubChannels(Resource):
         sysSettings = cachedDbCalls.getSystemSettings()
         channels = cachedDbCalls.getHubChannels()
         return  {"results": channels }, 200
+
+@api.route("/hubChannelsLive")
+class api_1_ActiveHubChannels(Resource):
+    @api.doc(responses={200: "Success", 400: "Request Error"})
+    def get(self):
+        """
+        Returns list of active channels
+        """
+        sysSettings = cachedDbCalls.getSystemSettings()
+        activeChannels = cachedDbCalls.getLiveChannels(hubCheck=False)
+        return  {"results": activeChannels }, 200
 
 # Invites Endpoint for a Channel
 @api.route("/<string:channelEndpointID>/streams")
